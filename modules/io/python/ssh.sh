ea29643a6112fede7622706d7098627c8ac0f03d
Tao He <linzhu.ht@alibaba-inc.com>
2020-11-23 15:40:22 +0800

    Fixes syntax.
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

--- modules/io/python/ssh.sh
+++ modules/io/python/ssh.sh
@@ -7,7 +7,7 @@ shift
 if [ "${HOST_NAME}" = "localhost" ] || [ "${HOST_NAME}" = "$(hostname)" ] || [ "${HOST_NAME}" = "$(hostname -i)" ];
 then
     shopt -s huponexit 2>/dev/null || true;
-    $*
+    bash -c "$*"
 else
     ssh ${HOST_NAME} -- "/bin/bash -c 'cat /etc/hosts > /dev/null || true && \
                                        source ~/.bashrc || true && \
