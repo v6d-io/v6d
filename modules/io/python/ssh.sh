21b5cb7bb9e6c0e5f1d1531d468913b4cfbeac9f
Tao He <linzhu.ht@alibaba-inc.com>
2020-11-23 13:53:10 +0800

    Bypass ssh remote execution if the hostname is localhost.
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

--- modules/io/python/ssh.sh
+++ modules/io/python/ssh.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 set -x
 HOST_NAME=$1
 shift
-ssh ${HOST_NAME} -- "/bin/bash -c 'cat /etc/hosts > /dev/null || true && \
-                                   source ~/.bashrc || true && \
-                                   shopt -s huponexit 2>/dev/null || true && \
-                                   $*'"
+
+if [ "${hostname}" = "localhost" ] || [ "${hostname}" = "$(hostname)" ] || [ "${hostname}" = "$(hostname -i)" ];
+then
+    shopt -s huponexit 2>/dev/null || true;
+    $*
+else
+    ssh ${HOST_NAME} -- "/bin/bash -c 'cat /etc/hosts > /dev/null || true && \
+                                       source ~/.bashrc || true && \
+                                       source ~/.bash_profile || true && \
+                                       shopt -s huponexit 2>/dev/null || true && \
+                                       $*'"
+fi
