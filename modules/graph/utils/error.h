e799915ee88f5ddb4fbecad2a0d70719bc142ec4
guanyi.gl <guanyi.gl@alibaba-inc.com>
2020-11-30 15:18:54 +0800

    fix

--- modules/graph/utils/error.h
+++ modules/graph/utils/error.h
@@ -113,16 +113,16 @@ inline grape::OutArchive& operator>>(grape::OutArchive& archive, GSError& e) {
 #define TOKENPASTE2(x, y) TOKENPASTE(x, y)
 
 #ifndef WITHOUT_LIBUNWIND
-#define RETURN_GS_ERROR(code, msg)                                            \
-  return ::boost::leaf::new_error(vineyard::GSError(                          \
-      (code), std::string(__FILE__) + ":" + std::to_string(__LINE__) + ": " + \
-                  std::string(__FUNCTION__) + " -> " + (msg)))
-#else
 #define RETURN_GS_ERROR(code, msg)                   \
   std::stringstream TOKENPASTE2(_ss, __LINE__);      \
   backtrace(TOKENPASTE2(_ss, __LINE__));             \
   return ::boost::leaf::new_error(vineyard::GSError( \
       (code), TOKENPASTE2(_ss, __LINE__).str() + " -> " + (msg)))
+#else
+#define RETURN_GS_ERROR(code, msg)                                            \
+  return ::boost::leaf::new_error(vineyard::GSError(                          \
+      (code), std::string(__FILE__) + ":" + std::to_string(__LINE__) + ": " + \
+                  std::string(__FUNCTION__) + " -> " + (msg)))
 #endif
 
 #define BOOST_LEAF_ASSIGN(v, r)                                     \
