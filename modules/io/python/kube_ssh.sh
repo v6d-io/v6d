b8d1e32c7222947366a59b3d70cfcb00fca78f30
Tao He <sighingnow@gmail.com>
2020-12-04 02:07:55 +0800

    Fixes launching in k8s environement. (#88)
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

--- modules/io/python/kube_ssh.sh
+++ modules/io/python/kube_ssh.sh
@@ -1,9 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 set -x
 POD_NAME=$1
 shift
-kubectl exec ${POD_NAME} -c engine -- "/bin/sh -c 'cat /etc/hosts > /dev/null || true && \
-                                                   source ~/.bashrc || true && \
-                                                   shopt -s huponexit 2>/dev/null || true && \
-                                                   $*'"
+
+if [[ "$POD_NAME" =~ ":" ]]; then
+    KUBE_NS=${POD_NAME%%:*}
+    KUBE_POD_NAME=${POD_NAME#*:}
+
+    kubectl -n ${KUBE_NS} exec ${KUBE_POD_NAME} -c engine -- /bin/sh -c "cat /etc/hosts > /dev/null || true && \
+                                                                         source ~/.bashrc || true && \
+                                                                         shopt -s huponexit 2>/dev/null || true && \
+                                                                         $*"
+else
+    kubectl exec ${POD_NAME} -c engine -- /bin/sh -c "cat /etc/hosts > /dev/null || true && \
+                                                      source ~/.bashrc || true && \
+                                                      shopt -s huponexit 2>/dev/null || true && \
+                                                      $*"
+fi
