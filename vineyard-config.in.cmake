46a9bb58dc77d45c7cabc790d58050d6d566ed49
Tao He <sighingnow@gmail.com>
2020-12-03 22:23:19 +0800

    Refine VINEYARD_HOME in cmake config. (#87)

--- vineyard-config.in.cmake
+++ vineyard-config.in.cmake
@@ -8,13 +8,14 @@
 #  VINEYARDD_EXECUTABLE         - the vineyardd executable
 #  VINEYARD_CODEGEN_EXECUTABLE  - the vineyard codegen executable
 
+set(VINEYARD_HOME "${CMAKE_CURRENT_LIST_DIR}/../../..")
 include("${CMAKE_CURRENT_LIST_DIR}/vineyard-targets.cmake")
 
 set(VINEYARD_LIBRARIES @VINEYARD_INSTALL_LIBS@)
-set(VINEYARD_INCLUDE_DIR "@CMAKE_INSTALL_PREFIX@/include"
-                         "@CMAKE_INSTALL_PREFIX@/include/vineyard")
+set(VINEYARD_INCLUDE_DIR "${VINEYARD_HOME}/include"
+                         "${VINEYARD_HOME}/include/vineyard")
 set(VINEYARD_INCLUDE_DIRS "${VINEYARD_INCLUDE_DIR}")
 
-set(VINEYARDD_EXECUTABLE "@CMAKE_INSTALL_PREFIX@/bin/vineyardd")
+set(VINEYARDD_EXECUTABLE "${VINEYARD_HOME}/bin/vineyardd")
 
-set(VINEYARD_CODEGEN_EXECUTABLE "@CMAKE_INSTALL_PREFIX@/bin/vineyard-codegen")
+set(VINEYARD_CODEGEN_EXECUTABLE "${VINEYARD_HOME}/bin/vineyard-codegen")
