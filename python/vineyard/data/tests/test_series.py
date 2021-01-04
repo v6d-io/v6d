db405504978ca192cfd80908ce258ffa5494e0bd
Weibin Zeng <qiaozi.zwb@alibaba-inc.com>
2021-01-04 14:49:47 +0800

    Implements builder and resolver for pandas.Series type (#118)

---
+++ python/vineyard/data/tests/test_series.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 Alibaba Group Holding Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import pandas as pd
+import pytest
+import numpy as np
+
+import vineyard
+from vineyard.core import default_builder_context, default_resolver_context
+from vineyard.data import register_builtin_types
+
+register_builtin_types(default_builder_context, default_resolver_context)
+
+
+def test_pandas_series(vineyard_client):
+    s = pd.Series([1, 3, 5, np.nan, 6, 8], name='foo')
+    object_id = vineyard_client.put(s)
+    pd.testing.assert_series_equal(s, vineyard_client.get(object_id))
