0f2bb49b1e5e42afaacacbd287d1e7d25e855ba1
Tao He <linzhu.ht@alibaba-inc.com>
2021-07-07 16:35:49 +0800

    Fixes the unpickleable issue of resolved `np.ndarray`.
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

--- python/vineyard/data/tensor.py
+++ python/vineyard/data/tensor.py
@@ -30,10 +30,20 @@ if pickle.HIGHEST_PROTOCOL < 5:
     import pickle5 as pickle
 
 from vineyard._C import ObjectMeta
-from .utils import from_json, to_json, build_numpy_buffer, normalize_dtype, normalize_cpptype, wrap_type
+from .utils import from_json, to_json, build_numpy_buffer, normalize_dtype, normalize_cpptype
+
 
 # Enable dynamic attribute on numpy.ndarray.
-ndarray_wrapped = wrap_type(np.ndarray)
+class ndarray(np.ndarray):
+    pass
+
+
+# n.b. don't wrap `__module__` otherwise `pickle` on such values will fail.
+#
+# ndarray.__module__ = np.ndarray.__module__
+ndarray.__name__ = np.ndarray.__name__
+ndarray.__qualname__ = np.ndarray.__qualname__
+ndarray.__doc__ = np.ndarray.__doc__
 
 
 def numpy_ndarray_builder(client, value, **kw):
@@ -67,7 +77,7 @@ def numpy_ndarray_resolver(obj):
     c_array = np.frombuffer(memoryview(obj.member('buffer_')), dtype=value_type).reshape(shape)
     # TODO: revise the memory copy of asfortranarray
     array = (c_array if order == 'C' else np.asfortranarray(c_array))
-    return array.view(ndarray_wrapped)
+    return array.view(ndarray)
 
 
 def bsr_matrix_builder(client, value, builder, **kw):
