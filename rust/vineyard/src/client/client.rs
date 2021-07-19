13878e616a2cbf4d9ab5be502b38439fa8a9e8e2
Siyuan Zhang <siyuanzhang.zsy@alibaba-inc.com>
2021-07-19 16:13:00 +0800

    Init rust sdk framework for client (#376)
    * Init rust sdk framework for client
    * fmt
    
    Signed-off-by: siyuan0322 <siyuanzhang.zsy@alibaba-inc.com>

---
+++ rust/vineyard/src/client/client.rs
@@ -0,0 +1,25 @@
+/** Copyright 2020-2021 Alibaba Group Holding Limited.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+use super::ObjectMeta;
+
+pub trait Client {
+    fn connect(&self, socket: &str) -> bool;
+
+    fn disconnect(&self);
+
+    fn connected(&self) -> bool;
+
+    fn get_meta_data(&self, object_id: u64, sync_remote: bool) -> ObjectMeta;
+}
