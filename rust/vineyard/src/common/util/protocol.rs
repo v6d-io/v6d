bb9e159ed1a35db932f962208e283c79f484a269
Sijie <lsjrosej@gmail.com>
2021-08-05 13:13:10 +0200

    update protocols
    Signed-off-by: Sijie <lsjrosej@gmail.com>

--- rust/vineyard/src/common/util/protocol.rs
+++ rust/vineyard/src/common/util/protocol.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
-use serde_json::{Value, json};
+use serde_json::{Value, Map, json};
 use serde_json::Result as JsonResult;
 
 use std::io::{self, ErrorKind, Error};
+use std::collections::{HashMap, HashSet};
 
 use super::{InstanceID, ObjectID};
 
@@ -13,11 +14,15 @@ enum CommandType {
     ExitReply,
 }
 
-pub struct Payload(usize);
+pub struct Payload{
+    object_id: ObjectID,
+}
 
 impl Payload {
     pub fn new() -> Payload{
-        Payload(0)
+        Payload{
+            object_id: 0,
+        }
     }
 
     pub fn to_json(&self) -> Value {
@@ -50,7 +55,7 @@ pub fn encode_msg(msg: Value) -> String {
     ret
 }
 
-// Write functions: Write JSON content to a String
+// Write functions: Derive and write JSON message to a String
 pub fn write_register_request() -> String {
     let msg = json!({"type": "register_request", "version": "0.2.6" });
     encode_msg(msg)
@@ -167,7 +172,7 @@ pub fn read_create_buffer_request(root: Value) -> Result<usize, Error> {
 }
 
 // TODO: Payload
-pub fn write_create_buffer_reply(id: ObjectID, object: Payload) -> String {
+pub fn write_create_buffer_reply(id: ObjectID, object: &Payload) -> String {
     let tree: Value = object.to_json();
     let msg = json!({"type": "create_buffer_reply", "id": id, "created": tree});
     encode_msg(msg)
@@ -177,12 +182,84 @@ pub fn read_create_buffer_reply(root: Value) -> Result<(ObjectID, Payload), Erro
     CHECK_IPC_ERROR(&root, "create_buffer_reply");
     let tree: Value = root["created"].clone();
     let id = root["id"].as_u64().unwrap() as ObjectID;
-    let object = Payload::new().from_json(&tree);
-    // let object = Payload::new(); 
-    // object.from_json(&tree);
+    let mut object = Payload::new(); 
+    object.from_json(&tree);
     Ok((id, object))
 }
 
+
+pub fn write_create_remote_buffer_request(size: usize) -> String {
+    let msg = json!({"type": "create_remote_buffer_request", "size": size});
+    encode_msg(msg)
+}
+
+pub fn read_create_remote_buffer_request(root: Value) -> Result<usize, Error> {
+    RETURN_ON_ASSERT(root["type"] == "create_remote_buffer_request");
+    let size = root["size"].as_u64().unwrap() as usize;
+    Ok(size)
+}
+
+pub fn write_get_buffer_request(ids: HashSet<ObjectID>) -> String {
+    let mut map = Map::new();
+    let mut idx: usize = 0;
+    // Q: Does root[std::to_string(idx++)] = id start from 0 right?
+    for id in &ids{
+        map.insert(idx.to_string(), Value::Number(serde_json::Number::from(*id)));
+        idx += 1;
+    }
+    map.insert(String::from("type"), Value::String("get_buffers_request".to_string()));
+    map.insert(String::from("num"), Value::Number(serde_json::Number::from(ids.len())));
+    let msg = Value::Object(map);
+    encode_msg(msg)
+}
+
+pub fn read_get_buffer_request(root: Value) -> Result<Vec<ObjectID>, Error> {
+    RETURN_ON_ASSERT(root["type"] == "get_buffers_request");
+    let mut ids: Vec<ObjectID> = Vec::new();
+    let num: usize = root["size"].as_u64().unwrap() as usize;
+    for idx in 0..num {
+        ids.push(root[idx.to_string()].as_u64().unwrap() as ObjectID)
+    }
+    Ok(ids)
+}
+
+pub fn write_get_buffer_reply(objects: Vec<Box<Payload>>) -> String {
+    let mut map = Map::new();
+    let num: usize = objects.len();
+    for idx in 0..num{
+        let tree: Value = objects[idx].to_json();
+        map.insert(idx.to_string(), tree);
+    }
+    map.insert(String::from("type"), Value::String("get_buffers_reply".to_string()));
+    map.insert(String::from("num"), Value::Number(serde_json::Number::from(objects.len())));
+    let msg = Value::Object(map);
+    encode_msg(msg)
+}
+
+pub fn read_get_buffer_reply(root: Value) -> Result<HashMap<ObjectID, Payload>, Error>  {
+    CHECK_IPC_ERROR(&root, "get_buffers_reply");
+    let mut objects: HashMap<ObjectID, Payload> = HashMap::new();
+    let num: usize = root["num"].as_u64().unwrap() as usize;
+    for idx in 0..num{
+        let tree: &Value = &root[idx.to_string()];
+        let mut object = Payload::new();
+        object.from_json(tree);
+        objects.insert(object.object_id, object);
+    }
+    Ok(objects)
+}
+
+// // Write functions: Write JSON content to a String
+// pub fn write_register_request() -> String {
+//     let msg = json!({"type": "register_request", "version": "0.2.6" });
+//     encode_msg(msg)
+// }
+
+// // Read functions: Read JSON root to variants of ipc instance
+// pub fn read_register_request(root: Value) -> Result<String, Error> {
+//     RETURN_ON_ASSERT(root["type"] == "register_request");
+//     Ok(root["version"].as_str().unwrap_or("0.0.0").to_string())
+// }
 
 
 #[cfg(test)]
