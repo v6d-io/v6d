v6d-io/v6d

commit d492bc086a4802c7a4dfbbc818219e8a5cd6eee1

Update dynamic hash partitioner (#506)
Signed-off-by: qiaozi.zwb <qiaozi.zwb@alibaba-inc.com>

modules/graph/utils/partitioner.h
@@ -114,10 +114,11 @@ class HashPartitioner<folly::dynamic> {
 
   inline fid_t GetPartitionId(const oid_t& oid) const {
     size_t hash_value;
-    if (oid.isString()) {
-      hash_value = std::hash<std::string>()(oid.getString());
+    if (oid.isArray() && oid.size == 2) {
+      // the oid is (label, id) format, just use id to get hash value.
+      hash_value = hash(oid[1]);
     } else {
-      hash_value = std::hash<folly::dynamic>()(oid);
+      hash_value = hash(oid);
     }
     return static_cast<fid_t>(static_cast<uint64_t>(hash_value) % fnum_);
   }
@@ -139,6 +140,14 @@ class HashPartitioner<folly::dynamic> {
   }
 
  private:
+  size_t hash(const oid_t& oid) const {
+    if (oid.isString()) {
+      return std::hash<std::string>()(oid.getString());
+    } else {
+      return std::hash<folly::dynamic>()(oid);
+    }
+  }
+
   fid_t fnum_;
 };
 #endif  // NETWORKX || EXPERIMENTAL_ON

commit 8a3563d89e2511468cdd6bf1121a0e5ad06c3d80

Fixes the invalid syntax in HashPartitioner for folly::Dynamic oids (#507)
The bug is introduced in #506.

Signed-off-by: qiaozi.zwb <qiaozi.zwb@alibaba-inc.com>

modules/graph/utils/partitioner.h
@@ -114,7 +114,7 @@ class HashPartitioner<folly::dynamic> {
 
   inline fid_t GetPartitionId(const oid_t& oid) const {
     size_t hash_value;
-    if (oid.isArray() && oid.size == 2) {
+    if (oid.isArray() && oid.size() == 2) {
       // the oid is (label, id) format, just use id to get hash value.
       hash_value = hash(oid[1]);
     } else {