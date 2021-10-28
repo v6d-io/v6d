v6d-io/v6d

commit 0135c99277f3f6723c2cca70047c0ca11d1cd1d3

Add more helpers in arrow utils. (#559)
+ EmptyTableBuilder::EmptySchema: generate empty schema.

Signed-off-by: Tao He <sighingnow@gmail.com>

modules/basic/ds/arrow_utils.cc
@@ -259,6 +259,11 @@ Status EmptyTableBuilder::Build(const std::shared_ptr<arrow::Schema>& schema,
   return Status::OK();
 }
 
+std::shared_ptr<arrow::Schema> EmptyTableBuilder::EmptySchema() {
+  return std::shared_ptr<arrow::Schema>(
+      new arrow::Schema({}, arrow::Endianness::Native));
+}
+
 std::shared_ptr<arrow::DataType> type_name_to_arrow_type(
     const std::string& name) {
   if (name == "bool") {

modules/basic/ds/arrow_utils.h
@@ -31,6 +31,7 @@ limitations under the License.
 #include "arrow/ipc/writer.h"
 #include "arrow/table.h"
 #include "arrow/table_builder.h"
+#include "arrow/type.h"
 #include "arrow/util/config.h"
 #include "glog/logging.h"
 
@@ -208,6 +209,8 @@ Status DeserializeTable(std::shared_ptr<arrow::Buffer> buffer,
 struct EmptyTableBuilder {
   static Status Build(const std::shared_ptr<arrow::Schema>& schema,
                       std::shared_ptr<arrow::Table>& table);
+
+  static std::shared_ptr<arrow::Schema> EmptySchema();
 };
 
 /**

commit d103dbdc188174668306ee26e6ed3b2b7e9dc5b2

Fixes backwards compatible with lower-version of apache-arrow. (#561)
Fixes compatible bug that introduced in #559.

Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

modules/basic/ds/arrow_utils.cc
@@ -260,8 +260,12 @@ Status EmptyTableBuilder::Build(const std::shared_ptr<arrow::Schema>& schema,
 }
 
 std::shared_ptr<arrow::Schema> EmptyTableBuilder::EmptySchema() {
+#if defined(ARROW_VERSION) && ARROW_VERSION >= 4000000
   return std::shared_ptr<arrow::Schema>(
       new arrow::Schema({}, arrow::Endianness::Native));
+#else
+  return std::shared_ptr<arrow::Schema>(new arrow::Schema({}));
+#endif
 }
 
 std::shared_ptr<arrow::DataType> type_name_to_arrow_type(