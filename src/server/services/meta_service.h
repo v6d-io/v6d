1ff91367462347cb1b295a5ebbaa1739fe825331
Tao He <linzhu.ht@alibaba-inc.com>
2021-12-01 14:12:31 +0800

    Bump up vineyard version to v0.3.12.
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

--- src/server/services/meta_service.h
+++ src/server/services/meta_service.h
@@ -172,9 +172,7 @@ class IMetaService {
       if (status.ok()) {
 #ifndef NDEBUG
         // debugging
-        if (VLOG_IS_ON(10)) {
-          printDepsGraph();
-        }
+        printDepsGraph();
 #endif
         this->metaUpdate(ops, false);
       } else {
@@ -750,9 +748,7 @@ class IMetaService {
 
 #ifndef NDEBUG
     // debugging
-    if (VLOG_IS_ON(10)) {
-      printDepsGraph();
-    }
+    printDepsGraph();
     for (auto const& id : blobs_to_delete) {
       LOG(INFO) << "blob to delete: " << ObjectIDToString(id);
     }
