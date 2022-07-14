2045d2493c658528d385ff0edd796d0eb934c0e6
ysm <yuanshumin@sjtu.edu.cn>
2022-07-14 18:02:42 +0800

    minor: refactor code
    Signed-off-by: ysm <yuanshumin@sjtu.edu.cn>

--- test/hosseinmoein_dataframe_test.cc
+++ test/hosseinmoein_dataframe_test.cc
@@ -1,3 +1,17 @@
+/** Copyright 2020-2021 Alibaba Group Holding Limited.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
@@ -36,13 +50,13 @@ std::ostream& operator<<(std::ostream& o, std::vector<T>& vec) {
 
 void init() {
   srand(time(0));
-  row = rand() % (MAX_ROW - 1) + 1;
+  row = rand() % (MAX_ROW - 1) + 1;  // NOLINT(runtime/threadsafe_fn)
 }
 
 template <typename T>
 void fill_random_data(std::vector<T>& vec) {
   for (int i = 0; i < row; i++) {
-    vec[i] = ((T)(rand() % MAX_DATA + 1)) / 3;
+    vec[i] = ((T)(rand() % MAX_DATA + 1)) / 3;  // NOLINT(runtime/threadsafe_fn)
   }
 }
 
