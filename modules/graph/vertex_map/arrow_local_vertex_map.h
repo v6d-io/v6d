96ccb1fec58ca395253347b693d6a4d6e3f19e2f
acezen <weibin.zen@gmail.com>
2022-08-10 11:25:45 +0800

    Fix
    Signed-off-by: acezen <weibin.zen@gmail.com>

--- modules/graph/vertex_map/arrow_local_vertex_map.h
+++ modules/graph/vertex_map/arrow_local_vertex_map.h
@@ -30,8 +30,8 @@ limitations under the License.
 #include "basic/ds/hashmap.h"
 #include "client/client.h"
 #include "common/util/functions.h"
+#include "common/util/static_if.h"
 #include "common/util/typename.h"
-#include "common/util/uuid.h"
 
 #include "graph/fragment/property_graph_types.h"
 #include "graph/fragment/property_graph_utils.h"
