d0d64875deacbd6b4aeccae014eea465d2e16e1d
Tao He <sighingnow@gmail.com>
2022-08-22 10:09:48 +0800

    Add docker images for vineyard installed and for vineyard developing (#929)
    Signed-off-by: Tao He <sighingnow@gmail.com>

---
+++ docker/dev/build_scripts/install-deps.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+export DEBCONF_NONINTERACTIVE_SEEN=true
+
+# install deps using apt-get
+apt-get update -y
+apt-get install -y sudo
+
+apt-get install -y \
+  ca-certificates \
+  ccache \
+  cmake \
+  curl \
+  doxygen \
+  fuse3 \
+  git \
+  libboost-all-dev \
+  libcurl4-openssl-dev \
+  libfuse3-dev \
+  libgflags-dev \
+  libgoogle-glog-dev \
+  libgmock-dev \
+  libgrpc-dev \
+  libgrpc++-dev \
+  libgtest-dev \
+  libkrb5-dev \
+  libmpich-dev \
+  libprotobuf-dev \
+  librdkafka-dev \
+  libgsasl7-dev \
+  librdkafka-dev \
+  libssl-dev \
+  libunwind-dev \
+  libxml2-dev \
+  libz-dev \
+  lsb-release \
+  pandoc \
+  protobuf-compiler-grpc \
+  python3-pip \
+  uuid-dev \
+  vim \
+  wget
+
+# apt-get cleanup
+apt-get autoclean
+rm -rf ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
+
