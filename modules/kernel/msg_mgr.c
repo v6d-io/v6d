4067f938876055d5062b03426f02c48aee7fd976
vegetableysm <108774481+vegetableysm@users.noreply.github.com>
2022-09-07 18:34:27 +0800

    Implemented a kernel module for vineyard to mount objects in vineyard as a file system directory (#964)
    * add kernel module base framework
    * add basic dir operations for vineyard fs
    * add basic framework of readdir
    * share buffer to user space
    * add wake/wait mechanism for kernel/user communication with netlink
    * add nl server for vineyard and connect it to kernel module
    * Interconnect the kernel module with Vineyard and implement readdir
    * implement lookup and read for vineyard fs
    * refactor kernel module
    * refactor netlink server
    * support user mount and optimize function implementation
    * refactor code
    * format code
    * make handler thread can be interrupt by signal
    * fix bug that can not read data if create object before mount
    * add macro for other plantform(except linux)
    * add macro for other plantform(except linux)
    * minor: add empty implementation of nl server for other platform(except linux)
    * minor: fix that vineyard failure to start when building code for non-Linux platforms
    * minor: When building plasma storage, cancel nl_server generation.
    * put the steps of synchronize the object info in readdir
    * put step of setting netlink macro into BUILD_VINEYARD_SERVER
    * add copyright in kernel module and format code
    * Format using the clang-format from Linux kernel
    
    Signed-off-by: ysm <yuanshumin@sjtu.edu.cn>

---
+++ modules/kernel/msg_mgr.c
@@ -0,0 +1,372 @@
+/** Copyright 2020-2022 Alibaba Group Holding Limited.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include <linux/stat.h>
+#include <linux/atmioc.h>
+#include <linux/slab.h>
+#include <linux/vmalloc.h>
+#include <linux/netlink.h>
+#include <net/sock.h>
+#include "vineyard_fs.h"
+#include "vineyard_i.h"
+#include "msg_mgr.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Vineyard authors");
+MODULE_DESCRIPTION("Vineyard filesystem for Linux.");
+MODULE_VERSION("0.01");
+
+#define NETLINK_VINEYARD 23
+#define USER_PORT 101
+
+#define PAGE_DOWN(x) ((x) & (~(PAGE_SIZE - 1)))
+#define PAGE_UP(x) PAGE_DOWN(x + PAGE_SIZE - 1)
+
+struct sock *nl_socket = NULL;
+extern struct net init_net;
+
+int vineyard_connect = 0;
+
+static struct page **pages;
+static unsigned long page_num;
+
+// vineyard bulk
+void *vineyard_storage_kernel_addr = NULL;
+void *vineyard_bulk_kernel_addr = NULL;
+
+// file entry buffer
+void *vineyard_object_info_kernel_addr = NULL;
+struct vineyard_object_info_header *vineyard_object_info_header;
+void *vineyard_object_info_buffer_addr;
+
+void *vineyard_object_info_user_addr = NULL;
+
+int msg_lock = 0;
+static struct vineyard_request_msg global_msg = { 0 };
+static struct vineyard_result_msg global_rmsg = { 0 };
+
+DECLARE_WAIT_QUEUE_HEAD(vineyard_msg_wait);
+DECLARE_WAIT_QUEUE_HEAD(vineyard_fs_wait);
+
+// tools
+static int send_msg(void)
+{
+	struct sk_buff *nl_skb;
+	struct nlmsghdr *nlh;
+	int ret;
+	unsigned long int length;
+	struct vineyard_msg msg;
+
+	length = sizeof(msg);
+	nl_skb = nlmsg_new(length, GFP_ATOMIC);
+
+	if (!nl_skb) {
+		printk(KERN_INFO PREFIX "Netlink alloc failure\n");
+	}
+
+	nlh = nlmsg_put(nl_skb, 0, 0, NETLINK_VINEYARD, length, 0);
+	if (!nlh) {
+		printk(KERN_INFO PREFIX "nlmsg_put failure\n");
+		nlmsg_free(nl_skb);
+		return -1;
+	}
+
+	memcpy(&msg.msg.request, &global_msg,
+	       sizeof(struct vineyard_request_msg));
+	global_msg.has_msg = 0;
+	memcpy(nlmsg_data(nlh), &msg, sizeof(msg));
+	printk(KERN_INFO "opt:%d\n", global_msg.opt);
+
+	ret = netlink_unicast(nl_socket, nl_skb, USER_PORT, MSG_DONTWAIT);
+
+	return ret;
+}
+
+static int map_vineyard_user_storage_buffer(unsigned long user_addr)
+{
+	struct vm_area_struct *user_vma;
+	int i;
+	int ret;
+
+	printk(KERN_INFO PREFIX "%s\n", __func__);
+	pages = vmalloc(page_num * sizeof(struct page *));
+	memset(pages, 0, sizeof(struct page *) * page_num);
+
+	user_vma = find_vma(current->mm, user_addr);
+	printk(KERN_INFO "addr: %lx\n", user_addr);
+	ret = get_user_pages_fast(user_addr, page_num, 0, pages);
+	if (ret < 0) {
+		printk(KERN_INFO "pined page error %d\n", ret);
+		vfree(pages);
+		return -1;
+	}
+
+	vineyard_storage_kernel_addr =
+		vmap(pages, page_num, VM_MAP, PAGE_KERNEL);
+
+	if (!vineyard_storage_kernel_addr) {
+		printk(KERN_INFO PREFIX "map error!\n");
+		for (i = 0; i < ret; i++) {
+			put_page(pages[i]);
+		}
+		vfree(pages);
+		return -1;
+	}
+
+	return 0;
+}
+
+static unsigned long prepare_user_buffer(void **kernel_addr, size_t size)
+{
+	unsigned long user_addr;
+	struct vm_area_struct *vma;
+
+	*kernel_addr = kzalloc(size, GFP_KERNEL);
+	if (!(*kernel_addr)) {
+		printk(KERN_INFO PREFIX "kmalloc error!\n");
+		return 0;
+	}
+
+	user_addr =
+		vm_mmap(NULL, 0, size, PROT_READ | PROT_WRITE, MAP_SHARED, 0);
+	vma = find_vma(current->mm, user_addr);
+	remap_pfn_range(vma, user_addr, PFN_DOWN(__pa(*kernel_addr)), size,
+			vma->vm_page_prot);
+	printk(KERN_INFO PREFIX "kern:%px\n", kernel_addr);
+
+	return user_addr;
+}
+
+static void unmap_vineyard_msg_result_buffer(void)
+{
+	kfree(vineyard_object_info_kernel_addr);
+}
+
+static void unmap_vineyard_user_storage_buffer(void)
+{
+	int i;
+
+	printk(KERN_INFO PREFIX "%s\n", __func__);
+	if (vineyard_storage_kernel_addr) {
+		printk(KERN_INFO "unmap and put pages\n");
+		vunmap(vineyard_storage_kernel_addr);
+		for (i = 0; i < page_num; i++) {
+			put_page(pages[i]);
+		}
+		vfree(pages);
+		vineyard_storage_kernel_addr = NULL;
+	}
+}
+
+static void init_object_info_buffer(void)
+{
+	vineyard_object_info_user_addr = (void *)prepare_user_buffer(
+		&vineyard_object_info_kernel_addr, PAGE_SIZE);
+	vineyard_object_info_header = vineyard_object_info_kernel_addr;
+	vineyard_object_info_buffer_addr =
+		vineyard_object_info_kernel_addr +
+		sizeof(struct vineyard_object_info_header);
+	global_msg.param._set_param.obj_info_mem =
+		(uint64_t)vineyard_object_info_user_addr;
+}
+
+static void handle_wait(void)
+{
+	int ret;
+
+	if (unlikely(vineyard_connect == 0))
+		vineyard_connect = 1;
+
+	// do {
+	ret = wait_event_interruptible(vineyard_msg_wait, global_msg.has_msg);
+	// } while (ret != 0);
+	if (ret < 0) {
+		global_msg.opt = VINEYARD_EXIT;
+		global_msg.has_msg = 1;
+	}
+
+	if (unlikely(global_msg.opt == VINEYARD_EXIT)) {
+		vineyard_connect = 0;
+		unmap_vineyard_user_storage_buffer();
+		unmap_vineyard_msg_result_buffer();
+		vineyard_spin_unlock(&msg_lock);
+	} else if (unlikely(global_msg.opt == VINEYARD_MOUNT)) {
+		init_object_info_buffer();
+	}
+
+	send_msg();
+	printk(KERN_INFO PREFIX "wait end!\n");
+}
+
+static void handle_set_bulk(struct vineyard_result_msg *rmsg)
+{
+	int err;
+	uint64_t bulk_addr;
+	uint64_t bulk_size;
+
+	bulk_addr = rmsg->ret._set_ret.bulk_addr;
+	bulk_size = rmsg->ret._set_ret.bulk_size;
+	printk(KERN_INFO PREFIX "%llx %llx %d\n", bulk_addr, bulk_size,
+	       rmsg->ret._set_ret.ret);
+
+	page_num = (PAGE_UP(bulk_addr + bulk_size) - PAGE_DOWN(bulk_addr)) /
+		   PAGE_SIZE;
+	printk(KERN_INFO PREFIX "page_num:%lu size:%lu\n", page_num,
+	       page_num * 0x1000);
+	err = map_vineyard_user_storage_buffer(PAGE_DOWN(bulk_addr));
+
+	vineyard_bulk_kernel_addr =
+		bulk_addr - PAGE_DOWN(bulk_addr) + vineyard_storage_kernel_addr;
+	printk(KERN_INFO PREFIX "vineyard kernel addr:%px storage:%px\n",
+	       vineyard_bulk_kernel_addr, vineyard_storage_kernel_addr);
+	printk(KERN_INFO PREFIX "err:%d\n", err);
+
+	memcpy(&global_rmsg, rmsg, sizeof(*rmsg));
+	global_rmsg.has_msg = 1;
+	global_rmsg.ret._set_ret.ret |= err;
+	wake_up(&vineyard_fs_wait);
+	handle_wait();
+}
+
+static void handle_fopt(struct vineyard_result_msg *rmsg)
+{
+	printk(KERN_INFO PREFIX "%s\n", __func__);
+	memcpy(&global_rmsg, rmsg, sizeof(*rmsg));
+	global_rmsg.has_msg = 1;
+	//1. send handler result to user
+	wake_up(&vineyard_fs_wait);
+	//2. call handler_wait
+	handle_wait();
+}
+
+static void netlink_rcv_msg(struct sk_buff *skb)
+{
+	struct nlmsghdr *nlh = NULL;
+	// struct vineyard_kern_user_msg *umsg = NULL;
+	struct vineyard_msg *rmsg;
+
+	if (skb->len >= nlmsg_total_size(0)) {
+		nlh = nlmsg_hdr(skb);
+		rmsg = (struct vineyard_msg *)NLMSG_DATA(nlh);
+		if (rmsg) {
+			printk(KERN_INFO PREFIX "Kernel recv from user %s\n",
+			       (char *)rmsg);
+
+			switch (rmsg->msg.result.opt) {
+			case VINEYARD_WAIT:
+				printk(KERN_INFO PREFIX "Receive opt: WAIT\n");
+				handle_wait();
+				break;
+			case VINEYARD_SET_BULK_ADDR:
+				printk(KERN_INFO PREFIX "Receive opt: VWAIT\n");
+				handle_set_bulk(&rmsg->msg.result);
+				break;
+			case VINEYARD_OPEN:
+			case VINEYARD_READ:
+			case VINEYARD_WRITE:
+			case VINEYARD_CLOSE:
+			case VINEYARD_FSYNC:
+			case VINEYARD_READDIR:
+				printk(KERN_INFO PREFIX "Receive opt: VFOPT\n");
+				handle_fopt(&rmsg->msg.result);
+				break;
+			default:
+				printk(KERN_INFO PREFIX "error!\n");
+				break;
+			}
+		}
+	}
+}
+
+struct netlink_kernel_cfg cfg = {
+	.input = netlink_rcv_msg,
+};
+
+int net_link_init(void)
+{
+	nl_socket = (struct sock *)netlink_kernel_create(
+		&init_net, NETLINK_VINEYARD, &cfg);
+	if (!nl_socket) {
+		printk(KERN_INFO PREFIX "Create netlink error!\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+void net_link_release(void)
+{
+	if (nl_socket) {
+		netlink_kernel_release(nl_socket);
+		nl_socket = NULL;
+	}
+}
+
+// interfaces
+void vineyard_spin_lock(volatile unsigned int *addr)
+{
+	while (!(__sync_bool_compare_and_swap(addr, 0, 1))) {
+	}
+}
+
+void vineyard_spin_unlock(volatile unsigned int *addr)
+{
+	*addr = 0;
+}
+
+void inline vineyard_read_lock(struct vineyard_rw_lock *rw_lock)
+{
+	vineyard_spin_lock(&rw_lock->w_lock);
+	rw_lock->r_lock++;
+	printk(KERN_INFO PREFIX "now there exist(s) %d readder\n",
+	       rw_lock->r_lock);
+	vineyard_spin_unlock(&rw_lock->w_lock);
+}
+
+void inline vineyard_read_unlock(struct vineyard_rw_lock *rw_lock)
+{
+	rw_lock->r_lock--;
+	printk(KERN_INFO PREFIX "now there exist(s) %d readder\n",
+	       rw_lock->r_lock);
+}
+
+int send_request_msg(struct vineyard_request_msg *msg)
+{
+	if (!vineyard_connect) {
+		return -1;
+	}
+	vineyard_spin_lock(&msg_lock);
+	memcpy(&global_msg, msg, sizeof(*msg));
+	global_msg.has_msg = 1;
+	wake_up(&vineyard_msg_wait);
+
+	return 0;
+}
+
+int receive_result_msg(struct vineyard_result_msg *rmsg)
+{
+	int ret = 0;
+
+	ret = wait_event_interruptible(vineyard_fs_wait, global_rmsg.has_msg);
+	if (ret != 0) {
+		global_rmsg.has_msg = 0;
+		vineyard_spin_unlock(&msg_lock);
+		return ret;
+	}
+
+	memcpy(rmsg, &global_rmsg, sizeof(*rmsg));
+	global_rmsg.has_msg = 0;
+	vineyard_spin_unlock(&msg_lock);
+	return ret;
+}
---
+++ src/server/async/netlink_server.cc
@@ -0,0 +1,332 @@
+/** Copyright 2020-2022 Alibaba Group Holding Limited.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "server/async/netlink_server.h"
+
+#if defined(BUILD_NETLINK_SERVER) && BUILD_NETLINK_SERVER
+#ifdef __linux__
+#include <limits>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <utility>
+#include <vector>
+
+#include "boost/filesystem/operations.hpp"
+#include "boost/filesystem/path.hpp"
+
+#include "common/util/json.h"
+#include "common/util/logging.h"
+#include "common/util/protocols.h"
+#include "server/server/vineyard_server.h"
+
+#define OFF (0)
+#define ON (1)
+#define DEBUG OFF
+#define PAGE_SIZE (0x1000)
+#define PAGE_DOWN(x) ((x) & (~(PAGE_SIZE - 1)))
+namespace vineyard {
+
+// Debug tools
+static void PrintJsonElement(const json& tree) {
+#if DEBUG == ON
+  LOG(INFO) << __func__;
+  for (auto iter = tree.begin(); iter != tree.end(); iter++) {
+    LOG(INFO) << *iter;
+  }
+#endif
+}
+
+NetLinkServer::NetLinkServer(std::shared_ptr<VineyardServer> vs_ptr)
+    : SocketServer(vs_ptr),
+      nlh(nullptr),
+      obj_info_mem(nullptr),
+      base_object_id(std::numeric_limits<uintptr_t>::max()) {
+  LOG(INFO) << __func__;
+}
+
+NetLinkServer::~NetLinkServer() {
+  LOG(INFO) << __func__;
+  close(socket_fd);
+  free(nlh);
+}
+
+void NetLinkServer::InitNetLink() {
+  size_t size;
+  socket_fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_VINEYARD);
+  if (socket_fd < 0) {
+    LOG(INFO) << "If you want to use netlink server, please insert kernel "
+                 "module first!";
+    return;
+  }
+  memset(&saddr, 0, sizeof(saddr));
+
+  saddr.nl_family = AF_NETLINK;
+  saddr.nl_pid = NETLINK_PORT;
+  saddr.nl_groups = 0;
+
+  if (bind(socket_fd, (struct sockaddr*) &saddr, sizeof(saddr)) != 0) {
+    LOG(INFO) << "NetLinkServer start error";
+    return;
+  }
+
+  memset(&daddr, 0, sizeof(daddr));
+  daddr.nl_family = AF_NETLINK;
+  daddr.nl_pid = 0;
+  daddr.nl_groups = 0;
+
+  size = sizeof(vineyard_msg);
+  nlh = (struct nlmsghdr*) malloc(NLMSG_SPACE(size));
+  memset(nlh, 0, sizeof(struct nlmsghdr));
+  nlh->nlmsg_len = NLMSG_SPACE(size);
+  nlh->nlmsg_flags = 0;
+  nlh->nlmsg_type = 0;
+  nlh->nlmsg_seq = 0;
+  nlh->nlmsg_pid = saddr.nl_pid;
+}
+
+uint64_t NetLinkServer::GetServerBulkField() {
+  // FIXME: it is temp implement.
+  std::vector<ObjectID> ids;
+  std::vector<std::shared_ptr<Payload>> objects;
+
+  ids.push_back(base_object_id);
+  vs_ptr_->GetBulkStore()->GetUnsafe(ids, true, objects);
+  if (!objects.empty())
+    return (uint64_t)(objects[0]->pointer);
+  return 0;
+}
+
+uint64_t NetLinkServer::GetServerBulkSize() {
+  std::vector<ObjectID> ids;
+  std::vector<std::shared_ptr<Payload>> objects;
+
+  ids.push_back(base_object_id);
+  vs_ptr_->GetBulkStore()->GetUnsafe(ids, true, objects);
+  if (!objects.empty())
+    return (uint64_t)(objects[0]->data_size);
+  return 0;
+}
+
+void NetLinkServer::InitialBulkField() {
+  base_pointer = reinterpret_cast<void*>(GetServerBulkField());
+}
+
+void NetLinkServer::Start() {
+  InitNetLink();
+  InitialBulkField();
+
+  work = new std::thread(thread_routine, this, socket_fd, saddr, daddr, nlh);
+}
+
+void NetLinkServer::Close() { LOG(INFO) << __func__; }
+
+void NetLinkServer::SyncObjectEntryList() {
+  vineyard_object_info_header* header;
+
+  header = reinterpret_cast<vineyard_object_info_header*>(obj_info_mem);
+  if (header) {
+    VineyardWriteLock(&header->rw_lock.r_lock, &header->rw_lock.w_lock);
+    vs_ptr_->ListData("vineyard::Blob", false,
+                      std::numeric_limits<size_t>::max(),
+                      [this, header](const Status& status, const json& tree) {
+                        if (!tree.empty()) {
+                          PrintJsonElement(tree);
+                          this->FillFileEntryInfo(tree, OBJECT_TYPE::BLOB);
+                        } else {
+                          header->total_file = 0;
+                        }
+                        VineyardWriteUnlock(&header->rw_lock.w_lock);
+                        return Status::OK();
+                      });
+  }
+}
+
+void NetLinkServer::doAccept() { LOG(INFO) << __func__; }
+
+int NetLinkServer::HandleSet(vineyard_request_msg* msg) {
+  LOG(INFO) << __func__;
+  obj_info_mem = reinterpret_cast<void*>(msg->param._set_param.obj_info_mem);
+  SyncObjectEntryList();
+  return 0;
+}
+
+fopt_ret NetLinkServer::HandleOpen(fopt_param& param) {
+  LOG(INFO) << __func__;
+
+  std::vector<ObjectID> ids;
+  std::vector<std::shared_ptr<Payload>> objects;
+  fopt_ret ret;
+  void* pointer = NULL;
+  uint64_t file_size = 0;
+
+  ret.ret = -1;
+  ids.push_back(param.obj_id);
+  vs_ptr_->GetData(
+      ids, false, false, []() { return true; },
+      [](const Status& status, const json& tree) {
+        LOG(INFO) << tree;
+        return Status::OK();
+      });
+
+  vs_ptr_->GetBulkStore()->GetUnsafe(ids, true, objects);
+  for (auto iter = objects.begin(); iter != objects.end(); iter++) {
+    pointer = (*iter)->pointer;
+    file_size = (*iter)->data_size;
+  }
+
+  if (pointer) {
+    ret.offset = (uint64_t) pointer - (uint64_t) base_pointer;
+    ret.ret = 0;
+    ret.size = file_size;
+  }
+
+  return ret;
+}
+
+fopt_ret NetLinkServer::HandleRead(fopt_param& param) {
+  LOG(INFO) << __func__;
+  fopt_ret ret;
+  ret.ret = 0;
+
+  return ret;
+}
+
+fopt_ret NetLinkServer::HandleWrite() {
+  fopt_ret ret;
+  ret.ret = 0;
+  return ret;
+}
+
+fopt_ret NetLinkServer::HandleCloseOrFsync() {
+  fopt_ret ret;
+  ret.ret = 0;
+  return ret;
+}
+
+fopt_ret NetLinkServer::HandleReadDir() {
+  SyncObjectEntryList();
+  fopt_ret ret;
+  ret.ret = 0;
+  return ret;
+}
+
+fopt_ret NetLinkServer::HandleFops(vineyard_request_msg* msg) {
+  fopt_ret ret;
+  switch (msg->opt) {
+  case MSG_OPT::VINEYARD_OPEN:
+    return HandleOpen(msg->param._fopt_param);
+  case MSG_OPT::VINEYARD_READ:
+    return HandleRead(msg->param._fopt_param);
+  case MSG_OPT::VINEYARD_WRITE:
+    return HandleWrite();
+  case MSG_OPT::VINEYARD_CLOSE:
+  case MSG_OPT::VINEYARD_FSYNC:
+    return HandleCloseOrFsync();
+  case MSG_OPT::VINEYARD_READDIR:
+    return HandleReadDir();
+  default:
+    LOG(INFO) << "Error opt!";
+    return ret;
+  }
+}
+
+void NetLinkServer::FillFileEntryInfo(const json& tree, enum OBJECT_TYPE type) {
+  LOG(INFO) << __func__;
+  vineyard_object_info_header* header;
+  vineyard_entry* entrys;
+  int i = 0;
+
+  header = reinterpret_cast<vineyard_object_info_header*>(obj_info_mem);
+  entrys = reinterpret_cast<vineyard_entry*>(header + 1);
+  PrintJsonElement(tree);
+
+  if (type == OBJECT_TYPE::BLOB) {
+    for (auto iter = tree.begin(); iter != tree.end(); iter++) {
+      entrys[i].obj_id = ObjectIDFromString((*iter)["id"].get<std::string>());
+      entrys[i].file_size = (*iter)["length"].get<uint64_t>();
+      entrys[i].type = type;
+      i++;
+    }
+  }
+  header->total_file = i;
+}
+
+void NetLinkServer::thread_routine(NetLinkServer* ns_ptr, int socket_fd,
+                                   struct sockaddr_nl saddr,
+                                   struct sockaddr_nl daddr,
+                                   struct nlmsghdr* nlh) {
+  LOG(INFO) << __func__;
+  int ret;
+  socklen_t len;
+  kmsg kmsg;
+  vineyard_result_msg umsg;
+  fopt_ret _fopt_ret;
+
+  if (socket_fd < 0) {
+    return;
+  }
+
+  memset(&umsg, 0, sizeof(umsg));
+  umsg.opt = MSG_OPT::VINEYARD_WAIT;
+
+  while (1) {
+    if (!nlh) {
+      LOG(INFO) << "If you want to use netlink server, please insert kernel "
+                   "module first!";
+      goto out;
+    }
+    memcpy(NLMSG_DATA(nlh), &umsg, sizeof(umsg));
+    ret = sendto(socket_fd, nlh, nlh->nlmsg_len, 0, (struct sockaddr*) &daddr,
+                 sizeof(struct sockaddr_nl));
+    if (!ret) {
+      LOG(INFO) << "NetLinkServer send msg error";
+      goto out;
+    }
+
+    memset(&kmsg, 0, sizeof(kmsg));
+    len = sizeof(struct sockaddr_nl);
+    ret = recvfrom(socket_fd, &kmsg, sizeof(kmsg), 0, (struct sockaddr*) &daddr,
+                   &len);
+    if (!ret) {
+      LOG(INFO) << "Recv form kernel error\n";
+      goto out;
+    }
+
+    switch (kmsg.msg.msg.request.opt) {
+    case MSG_OPT::VINEYARD_MOUNT:
+      ns_ptr->HandleSet(&kmsg.msg.msg.request);
+      umsg.ret._set_ret.bulk_addr = ns_ptr->GetServerBulkField();
+      umsg.ret._set_ret.bulk_size = ns_ptr->GetServerBulkSize();
+      umsg.ret._set_ret.ret = 0;
+      umsg.opt = MSG_OPT::VINEYARD_SET_BULK_ADDR;
+      break;
+    case MSG_OPT::VINEYARD_EXIT:
+      LOG(INFO) << "Bye! Handler thread exit!";
+      goto out;
+    default:
+      _fopt_ret = ns_ptr->HandleFops(&kmsg.msg.msg.request);
+      memcpy(&umsg.ret._fopt_ret, &_fopt_ret, sizeof(fopt_ret));
+      umsg.opt = kmsg.msg.msg.request.opt;
+      break;
+    }
+  }
+
+out:
+  return;
+}
+
+}  // namespace vineyard
+#endif  // __linux__
+#endif  // BUILD_NETLINK_SERVER
