ac6a7674e53d89e7e61a8b36c557060b44f47bf9
acezen <weibin.zen@gmail.com>
2022-10-19 00:39:29 +0800

    update

--- modules/graph/loader/arrow_fragment_writer.h
+++ modules/graph/loader/arrow_fragment_writer.h
@@ -52,7 +52,7 @@ struct WriterConfig {
     edge_chunk_size = other.edge_chunk_size;
     vertex_chunk_file_type = other.vertex_chunk_file_type;
     edge_chunk_file_type = other.edge_chunk_file_type;
-    adj_list_type = other.adj_list_type;
+    adj_list_types = other.adj_list_types;
     yaml_output_path = other.yaml_output_path;
   }
 
@@ -61,7 +61,7 @@ struct WriterConfig {
   int edge_chunk_size;
   gsf::FileType vertex_chunk_file_type;
   gsf::FileType edge_chunk_file_type;
-  gsf::AdjListType adj_list_type;
+  std::vector<gsf::AdjListType> adj_list_types;
   std::string yaml_output_path;
 };
 
@@ -77,17 +77,12 @@ class ArrowFragmentWriter {
   using vid_t = VID_T;
   using eid_t = property_graph_types::EID_TYPE;
   using label_id_t = vineyard::property_graph_types::LABEL_ID_TYPE;
+  using vertex_range_t = grape::VertexRange<vid_t>;
   using internal_oid_t = typename vineyard::InternalType<oid_t>::type;
   using oid_array_t = typename vineyard::ConvertToArrowType<oid_t>::ArrayType;
   using vertex_map_t = vineyard::ArrowVertexMap<internal_oid_t, vid_t>;
   using oid_array_builder_t = typename vineyard::ConvertToArrowType<oid_t>::BuilderType;
-  using range_t = std::pair<oid_t, oid_t>;
   using nbr_t = property_graph_utils::Nbr<vid_t, eid_t>;
-  static constexpr const char* LABEL_TAG = "label";
-  static constexpr const char* SRC_LABEL_TAG = "src_label";
-  static constexpr const char* DST_LABEL_TAG = "dst_label";
-
-  const int id_column = 0;
 
  public:
   ArrowFragmentWriter(const std::shared_ptr<ArrowFragment<OID_T, VID_T>> frag,
@@ -115,7 +110,7 @@ class ArrowFragmentWriter {
         gsf::Property property = {prop_def.name, gsf::Type::arrow_data_type_to_type(prop_def.type), false};
         properties.push_back(property);
       }
-      gsf::PropertyGroup group = {"properties", config_.vertex_chunk_file_type, properties};
+      gsf::PropertyGroup group = {"", config_.vertex_chunk_file_type, properties};
       CHECK(vertex_info.AddPropertyGroup(group).ok());
       graph_info->AddVertex(vertex_info);
     }
@@ -126,11 +121,13 @@ class ArrowFragmentWriter {
         gsf::Property property = {prop_def.name, gsf::Type::arrow_data_type_to_type(prop_def.type), false};
         properties.push_back(property);
       }
-      gsf::PropertyGroup group = {"properties", config_.edge_chunk_file_type, properties};
+      gsf::PropertyGroup group = {"", config_.edge_chunk_file_type, properties};
       for (auto& relation : entry.relations) {
         gsf::EdgeInfo edge_info(relation.first, edge_label, relation.second, config_.edge_chunk_size, config_.vertex_chunk_size, config_.vertex_chunk_size, directed_, "edge/");
-        CHECK(edge_info.AddAdjList(config_.adj_list_type, config_.edge_chunk_file_type).ok());
-        CHECK(edge_info.AddPropertyGroup(group, config_.adj_list_type).ok());
+        for (auto& adj_list_type : config_.adj_list_types) {
+          CHECK(edge_info.AddAdjList(adj_list_type, config_.edge_chunk_file_type).ok());
+          CHECK(edge_info.AddPropertyGroup(group, adj_list_type).ok());
+        }
         graph_info->AddEdge(edge_info);
       }
     }
@@ -144,6 +141,30 @@ class ArrowFragmentWriter {
     }
     WriteVertexChunks();
     WriteEdgeChunks();
+    SaveGraphInfo();
+  }
+
+  void SaveGraphInfo() {
+    auto maybe_fs = gsf::FileSystemFromUriOrPath(config_.yaml_output_path);
+    if (!maybe_fs.status().ok()) {
+      LOG(ERROR) << "Failed to get file system from uri: " << maybe_fs.status().message();
+    }
+    auto fs = maybe_fs.value();
+    for (auto& pair : graph_info_->GetAllVertexInfo()) {
+      auto& label = pair.first;
+      auto& vertex_info = pair.second;
+      std::string vertex_info_path = config_.yaml_output_path + "/" + label + ".vertex.yml";
+      vertex_info.Save(fs, vertex_info_path);
+      graph_info_->AddVertexInfoPath(vertex_info.GetLabel() + ".vertex.yml");
+    }
+    for (auto& pair : graph_info_->GetAllEdgeInfo()) {
+      auto& concat_label = pair.first;
+      auto& edge_info = pair.second;
+      std::string edge_info_path = config_.yaml_output_path + "/" + concat_label + ".edge.yml";
+      edge_info.Save(fs, edge_info_path);
+      graph_info_->AddEdgeInfoPath(concat_label + ".edge.yml");
+    }
+    graph_info_->Save(fs, config_.yaml_output_path + "/" + name_ + ".graph.yml");
   }
 
   void WriteVertexChunks() {
@@ -154,7 +175,7 @@ class ArrowFragmentWriter {
     auto& schema = frag_->schema();
     auto vm_ptr = frag_->GetVertexMap();
     for (label_id_t label = 0; label < frag_->vertex_label_num(); ++label) {
-      // set the start chunk index
+      // set the start chunk index of fragment
       gsf::IdType start_chunk_index = 0;
       for (fid_t fid = 0; fid < frag_->fid(); ++fid) {
         if (vm_ptr->GetInnerVertexSize(fid) % config_.vertex_chunk_size == 0 ) {
@@ -179,33 +200,45 @@ class ArrowFragmentWriter {
       return;
     }
 
-    auto& schema = frag_->schema();
     auto vm_ptr = frag_->GetVertexMap();
-    vineyard::IdParser<vid_t> vid_parser;
-    vid_parser.Init(frag_->fnum(), frag_->vertex_label_num());
     for (label_id_t label = 0; label < frag_->vertex_label_num(); ++label) {
       std::vector<gsf::IdType> start_chunk_indices(frag_->fnum(), 0);
       for (fid_t fid = 0; fid < frag_->fnum() - 1; ++fid) {
         if (vm_ptr->GetInnerVertexSize(fid) % config_.vertex_chunk_size == 0 ) {
-          start_chunk_indices[fid + 1] += vm_ptr->GetInnerVertexSize(fid) / config_.vertex_chunk_size;
+          start_chunk_indices[fid + 1] = start_chunk_indices[fid] + vm_ptr->GetInnerVertexSize(fid) / config_.vertex_chunk_size;
         } else {
-          start_chunk_indices[fid + 1] += vm_ptr->GetInnerVertexSize(fid) / config_.vertex_chunk_size + 1;
+          start_chunk_indices[fid + 1] = start_chunk_indices[fid] + vm_ptr->GetInnerVertexSize(fid) / config_.vertex_chunk_size + 1;
         }
       }
-      auto& start_chunk_index = start_chunk_indices[frag_->fid()];
       auto inner_vertices = frag_->InnerVertices(label);
+      auto& schema = frag_->schema();
       for (auto& pair : graph_info_->GetAllEdgeInfo()) {
         auto& edge_info = pair.second;
+        if (edge_info.ContainAdjList(gsf::AdjListType::ordered_by_source) && edge_info.GetSrcLabel() == schema.GetVertexLabelName(label)) {
+          writeCSR(edge_info, start_chunk_indices, inner_vertices);
+        }
+        if (edge_info.ContainAdjList(gsf::AdjListType::ordered_by_dest) && edge_info.GetDstLabel() == schema.GetVertexLabelName(label)) {
+          writeCSC(edge_info, start_chunk_indices, inner_vertices);
+        }
+      }
+    }
+  }
+
+ private:
+  void writeCSR(const gsf::EdgeInfo& edge_info, std::vector<gsf::IdType> start_chunk_indices, const vertex_range_t& vertices) {
+    vineyard::IdParser<vid_t> vid_parser;
+    vid_parser.Init(frag_->fnum(), frag_->vertex_label_num());
         auto edge_label = edge_info.GetEdgeLabel();
         auto src_label = edge_info.GetSrcLabel();
         auto dst_label = edge_info.GetDstLabel();
-        if (config_.adj_list_type == gsf::AdjListType::ordered_by_source && src_label == schema.GetVertexLabelName(label)) {
-          gsf::EdgeChunkWriter writer(edge_info, graph_info_->GetPrefix(), config_.adj_list_type);
+        auto start_chunk_index = start_chunk_indices[frag_->fid()];
+        auto& schema = frag_->schema();
+          gsf::EdgeChunkWriter writer(edge_info, graph_info_->GetPrefix(), gsf::AdjListType::ordered_by_source);
           int v_count = 0, e_count = 0;
           int v_chunk_index = start_chunk_index, e_chunk_index = 0;
           label_id_t e_label_id = schema.GetEdgeLabelId(edge_label);
           label_id_t prop_num = frag_->edge_property_num(e_label_id);
-          std::vector<std::shared_ptr<arrow::Array>> src_dst_columns(prop_num + 2);
+          std::vector<std::shared_ptr<arrow::Array>> edge_columns(prop_num + 2);
           std::vector<std::shared_ptr<arrow::Array>> offset_columns(1);
           std::vector<std::shared_ptr<arrow::ArrayBuilder>> builders(prop_num + 2);
           arrow::Int64Builder offset_builder;
@@ -221,7 +254,7 @@ class ArrowFragmentWriter {
             fields.push_back(arrow::field(prop_name, prop_type));
           }
           std::shared_ptr<arrow::Schema> table_schema = arrow::schema(fields);
-          for (auto& u : inner_vertices) {
+          for (auto& u : vertices) {
             auto u_global_id = start_chunk_index * config_.vertex_chunk_size + vid_parser.GetOffset(u.GetValue());
             auto oe = frag_->GetOutgoingAdjList(u, schema.GetEdgeLabelId(edge_label));
             for (auto& e : oe) {
@@ -240,9 +273,8 @@ class ArrowFragmentWriter {
               append_property_to_arrow_array_builders(builders, e, prop_num, e_label_id);
               e_count++;
               if (e_count == config_.edge_chunk_size) {
-                finish_arrow_array_builders(builders, src_dst_columns);
-                // auto edge_table = arrow::Table::Make(arrow::schema({arrow::field("src", arrow::int64()), arrow::field("dst", arrow::int64())}), src_dst_columns);
-                auto edge_table = arrow::Table::Make(table_schema, src_dst_columns);
+                finish_arrow_array_builders(builders, edge_columns);
+                auto edge_table = arrow::Table::Make(table_schema, edge_columns);
                 auto st = writer.WriteChunk(edge_table, v_chunk_index, e_chunk_index);
                 CHECK(st.ok());
                 reset_arrow_array_builders(builders);
@@ -256,10 +288,8 @@ class ArrowFragmentWriter {
             // one chunk size.
             if (v_count == config_.vertex_chunk_size) {
               if (e_count != 0) {
-                finish_arrow_array_builders(builders, src_dst_columns);
-                // auto edge_table = arrow::Table::Make(arrow::schema({arrow::field("src", arrow::int64()), arrow::field("dst", arrow::int64())}), src_dst_columns);
-                // auto st = writer.WriteAdjListChunk(edge_table, v_chunk_index, e_chunk_index);
-                auto edge_table = arrow::Table::Make(table_schema, src_dst_columns);
+                finish_arrow_array_builders(builders, edge_columns);
+                auto edge_table = arrow::Table::Make(table_schema, edge_columns);
                 auto st = writer.WriteChunk(edge_table, v_chunk_index, e_chunk_index);
                 if (!st.ok()) {
                   LOG(ERROR) << "write chunk error: " << st.message();
@@ -282,10 +312,8 @@ class ArrowFragmentWriter {
           // maybe the last chunk not align to vertex chunk size
           if (v_count != 0) {
               if (e_count != 0) {
-                finish_arrow_array_builders(builders, src_dst_columns);
-                // auto edge_table = arrow::Table::Make(arrow::schema({arrow::field("src", arrow::int64()), arrow::field("dst", arrow::int64())}), src_dst_columns);
-                // auto st = writer.WriteAdjListChunk(edge_table, v_chunk_index, e_chunk_index);
-                auto edge_table = arrow::Table::Make(table_schema, src_dst_columns);
+                finish_arrow_array_builders(builders, edge_columns);
+                auto edge_table = arrow::Table::Make(table_schema, edge_columns);
                 auto st = writer.WriteChunk(edge_table, v_chunk_index, e_chunk_index);
                 CHECK(st.ok());
                 reset_arrow_array_builders(builders);
@@ -298,12 +326,111 @@ class ArrowFragmentWriter {
               auto st = writer.WriteOffsetChunk(offset_table, v_chunk_index);
               CHECK(st.ok());
           }
-        }
-      }
-    }
   }
 
- private:
+  void writeCSC(const gsf::EdgeInfo& edge_info, std::vector<gsf::IdType> start_chunk_indices, const vertex_range_t& vertices) {
+    vineyard::IdParser<vid_t> vid_parser;
+    vid_parser.Init(frag_->fnum(), frag_->vertex_label_num());
+        auto edge_label = edge_info.GetEdgeLabel();
+        auto src_label = edge_info.GetSrcLabel();
+        auto dst_label = edge_info.GetDstLabel();
+        auto start_chunk_index = start_chunk_indices[frag_->fid()];
+        auto& schema = frag_->schema();
+          gsf::EdgeChunkWriter writer(edge_info, graph_info_->GetPrefix(), gsf::AdjListType::ordered_by_dest);
+          int v_count = 0, e_count = 0;
+          int v_chunk_index = start_chunk_index, e_chunk_index = 0;
+          label_id_t e_label_id = schema.GetEdgeLabelId(edge_label);
+          label_id_t prop_num = frag_->edge_property_num(e_label_id);
+          std::vector<std::shared_ptr<arrow::Array>> edge_columns(prop_num + 2);
+          std::vector<std::shared_ptr<arrow::Array>> offset_columns(1);
+          std::vector<std::shared_ptr<arrow::ArrayBuilder>> builders(prop_num + 2);
+          arrow::Int64Builder offset_builder;
+          construct_arrow_array_builders(builders, prop_num, e_label_id);
+          auto src_builder = std::dynamic_pointer_cast<arrow::Int64Builder>(builders[0]);
+          auto dst_builder = std::dynamic_pointer_cast<arrow::Int64Builder>(builders[1]);
+          int64_t offset = 0;
+          offset_builder.Append(offset);
+          std::vector<std::shared_ptr<arrow::Field>> fields = {arrow::field("src", arrow::int64()), arrow::field("dst", arrow::int64())};
+          for (int64_t i = 0; i < prop_num; ++i) {
+            auto prop_name = schema.GetEdgePropertyName(e_label_id, i);
+            auto prop_type = schema.GetEdgePropertyType(e_label_id, i);
+            fields.push_back(arrow::field(prop_name, prop_type));
+          }
+          std::shared_ptr<arrow::Schema> table_schema = arrow::schema(fields);
+          for (auto& u : vertices) {
+            auto u_global_id = start_chunk_index * config_.vertex_chunk_size + vid_parser.GetOffset(u.GetValue());
+            auto ie = frag_->GetIncomingAdjList(u, schema.GetEdgeLabelId(edge_label));
+            for (auto& e : ie) {
+              auto v = e.neighbor();
+              gsf::IdType v_global_id;
+              if (frag_->IsInnerVertex(v)) {
+                v_global_id = start_chunk_index * config_.vertex_chunk_size + vid_parser.GetOffset(v.GetValue());
+              } else {
+                auto gid = frag_->GetOuterVertexGid(v);
+                auto v_fid = vid_parser.GetFid(gid);
+                auto v_offset = vid_parser.GetOffset(gid);
+                v_global_id = start_chunk_indices[v_fid] * config_.vertex_chunk_size + v_offset;
+              }
+              src_builder->Append(v_global_id);
+              dst_builder->Append(u_global_id);
+              append_property_to_arrow_array_builders(builders, e, prop_num, e_label_id);
+              e_count++;
+              if (e_count == config_.edge_chunk_size) {
+                finish_arrow_array_builders(builders, edge_columns);
+                auto edge_table = arrow::Table::Make(table_schema, edge_columns);
+                auto st = writer.WriteChunk(edge_table, v_chunk_index, e_chunk_index);
+                CHECK(st.ok());
+                reset_arrow_array_builders(builders);
+                e_count = 0;
+                e_chunk_index++;
+              }
+            }
+            offset += ie.size();
+            offset_builder.Append(offset);
+            v_count++;
+            // one chunk size.
+            if (v_count == config_.vertex_chunk_size) {
+              if (e_count != 0) {
+                finish_arrow_array_builders(builders, edge_columns);
+                auto edge_table = arrow::Table::Make(table_schema, edge_columns);
+                auto st = writer.WriteChunk(edge_table, v_chunk_index, e_chunk_index);
+                if (!st.ok()) {
+                  LOG(ERROR) << "write chunk error: " << st.message();
+                }
+                CHECK(st.ok());
+                reset_arrow_array_builders(builders);
+                e_count = 0;
+                e_chunk_index = 0;
+              }
+              offset_builder.Finish(&offset_columns[0]);
+              auto offset_table = arrow::Table::Make(arrow::schema({arrow::field("offset", arrow::int64())}), offset_columns);
+              auto st = writer.WriteOffsetChunk(offset_table, v_chunk_index);
+              CHECK(st.ok());
+              offset_builder.Reset();
+              v_chunk_index++;
+              e_chunk_index = 0;
+              v_count = 0;
+            }
+          }
+          // maybe the last chunk not align to vertex chunk size
+          if (v_count != 0) {
+              if (e_count != 0) {
+                finish_arrow_array_builders(builders, edge_columns);
+                auto edge_table = arrow::Table::Make(table_schema, edge_columns);
+                auto st = writer.WriteChunk(edge_table, v_chunk_index, e_chunk_index);
+                CHECK(st.ok());
+                reset_arrow_array_builders(builders);
+                e_count = 0;
+                e_chunk_index = 0;
+              }
+              offset_builder.Finish(&offset_columns[0]);
+              auto offset_table = arrow::Table::Make(arrow::schema({arrow::field("offset", arrow::int64())}), offset_columns);
+              offset_builder.Reset();
+              auto st = writer.WriteOffsetChunk(offset_table, v_chunk_index);
+              CHECK(st.ok());
+          }
+  }
+
   void construct_arrow_array_builders(std::vector<std::shared_ptr<arrow::ArrayBuilder>>& builders, int64_t prop_num, label_id_t edge_label) {
     builders.resize(prop_num + 2);
     builders[0] = std::make_shared<arrow::Int64Builder>();
