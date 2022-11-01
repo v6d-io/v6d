ab83d14a65feef1c77c78c05296f20610249fc6c
Tao He <sighingnow@gmail.com>
2022-11-01 20:44:48 +0800

    Source more rf-files and use bash -lc when starting IO drivers
    Signed-off-by: Tao He <sighingnow@gmail.com>

--- modules/io/python/drivers/io/kube_ssh.sh
+++ modules/io/python/drivers/io/kube_ssh.sh
@@ -7,13 +7,21 @@ if [[ "$POD_NAME" =~ ":" ]]; then
     KUBE_NS=${POD_NAME%%:*}
     KUBE_POD_NAME=${POD_NAME#*:}
 
-    kubectl -n ${KUBE_NS} exec ${KUBE_POD_NAME} -c engine -- /bin/bash -c "cat /etc/hosts > /dev/null || true && \
-                                                                           source ~/.bashrc || true && \
-                                                                           shopt -s huponexit 2>/dev/null || true && \
-                                                                           $*"
+    kubectl -n ${KUBE_NS} exec ${KUBE_POD_NAME} -c engine -- /bin/bash -lc "
+        cat /etc/hosts > /dev/null || true && \
+        source /etc/profile || true && \
+        source /etc/bash.bashrc || true && \
+        source ~/.profile || true && \
+        source ~/.bashrc || true && \
+        shopt -s huponexit 2>/dev/null || true && \
+        $*"
 else
-    kubectl exec ${POD_NAME} -c engine -- /bin/bash -c "cat /etc/hosts > /dev/null || true && \
-                                                        source ~/.bashrc || true && \
-                                                        shopt -s huponexit 2>/dev/null || true && \
-                                                        $*"
+    kubectl exec ${POD_NAME} -c engine -- /bin/bash -lc "
+        cat /etc/hosts > /dev/null || true && \
+        source /etc/profile || true && \
+        source /etc/bash.bashrc || true && \
+        source ~/.profile || true && \
+        source ~/.bashrc || true && \
+        shopt -s huponexit 2>/dev/null || true && \
+        $*"
 fi
