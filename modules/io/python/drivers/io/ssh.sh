1e96b60dc87165187a2b237786a439ba97a9abb5
Tao He <linzhu.ht@alibaba-inc.com>
2022-11-01 20:45:38 +0800

    Source more rf-files and use bash -lc when starting IO drivers (#1043)
    Signed-off-by: Tao He <sighingnow@gmail.com>

--- modules/io/python/drivers/io/ssh.sh
+++ modules/io/python/drivers/io/ssh.sh
@@ -8,10 +8,13 @@ then
     shopt -s huponexit 2>/dev/null || true;
     bash -c "$*"
 else
-    ssh ${HOST_NAME} -- "/bin/bash -c 'cat /etc/hosts > /dev/null || true && \
-                                       source /etc/bash.bashrc || true && \
-                                       source ~/.bashrc || true && \
-                                       source ~/.bash_profile || true && \
-                                       shopt -s huponexit 2>/dev/null || true && \
-                                       $*'"
+    ssh ${HOST_NAME} -- "/bin/bash -lc '\
+        cat /etc/hosts > /dev/null || true && \
+        source /etc/profile || true && \
+        source /etc/bash.bashrc || true && \
+        source ~/.profile || true && \
+        source ~/.bashrc || true && \
+        source ~/.bash_profile || true && \
+        shopt -s huponexit 2>/dev/null || true && \
+        $*'"
 fi
