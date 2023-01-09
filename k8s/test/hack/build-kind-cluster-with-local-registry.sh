678cb080e171ab3eba6b74d3a08fc8cc07186eeb
Ye Cao <caoye.cao@alibaba-inc.com>
2023-01-09 11:14:43 +0800

    Build kind cluster with local registry (#1131)
    Signed-off-by: Ye Cao <caoye.cao@alibaba-inc.com>

---
+++ k8s/test/hack/build-kind-cluster-with-local-registry.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+set -o errexit
+
+kind_name=test
+
+if [ ! -z "$1" ] ; then
+    kind_name=$1
+fi
+# create registry container unless it already exists
+reg_name='kind-registry'
+reg_port='5001'
+if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
+    registry:2
+fi
+
+# create a cluster with the local registry enabled in containerd
+cat <<EOF | kind create cluster --name=${kind_name} --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
+    endpoint = ["http://${reg_name}:5000"]
+nodes:
+- role: control-plane
+  image: kindest/node:v1.24.0
+- role: worker
+  image: kindest/node:v1.24.0
+- role: worker
+  image: kindest/node:v1.24.0
+- role: worker
+  image: kindest/node:v1.24.0
+EOF
+
+# connect the registry to the cluster network if not already connected
+if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
+  docker network connect "kind" "${reg_name}"
+fi
+
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
