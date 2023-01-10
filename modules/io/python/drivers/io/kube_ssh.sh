ec92ebd0a7a99f1ba0554dcd00a8bd4569c1f4f0
Ye Cao <caoye.cao@alibaba-inc.com>
2023-01-10 18:01:21 +0800

    Add container name as a option to kube_ssh.sh
    Signed-off-by: Ye Cao <caoye.cao@alibaba-inc.com>

--- modules/io/python/drivers/io/kube_ssh.sh
+++ modules/io/python/drivers/io/kube_ssh.sh
@@ -5,16 +5,28 @@ shift
 
 if [[ "$POD_NAME" =~ ":" ]]; then
     KUBE_NS=${POD_NAME%%:*}
-    KUBE_POD_NAME=${POD_NAME#*:}
-
-    kubectl -n ${KUBE_NS} exec ${KUBE_POD_NAME} -c engine -- /bin/bash -lc "
-        cat /etc/hosts > /dev/null || true && \
-        source /etc/profile || true && \
-        source /etc/bash.bashrc || true && \
-        source ~/.profile || true && \
-        source ~/.bashrc || true && \
-        shopt -s huponexit 2>/dev/null || true && \
-        $*"
+    KUBE_POD_CONTAINER_NAME=${POD_NAME#*:}
+    if [[ "$KUBE_POD_CONTAINER_NAME" =~ ":" ]]; then
+        KUBE_POD_NAME=${KUBE_POD_CONTAINER_NAME%%:*}
+        KUBE_CONTAINER_NAME=${KUBE_POD_CONTAINER_NAME#*:}
+        kubectl -n ${KUBE_NS} exec ${KUBE_POD_NAME} -c ${KUBE_CONTAINER_NAME} -- /bin/bash -lc "
+            cat /etc/hosts > /dev/null || true && \
+            source /etc/profile || true && \
+            source /etc/bash.bashrc || true && \
+            source ~/.profile || true && \
+            source ~/.bashrc || true && \
+            shopt -s huponexit 2>/dev/null || true && \
+            $*"
+    else
+        kubectl -n ${KUBE_NS} exec ${KUBE_POD_CONTAINER_NAME} -c engine -- /bin/bash -lc "
+            cat /etc/hosts > /dev/null || true && \
+            source /etc/profile || true && \
+            source /etc/bash.bashrc || true && \
+            source ~/.profile || true && \
+            source ~/.bashrc || true && \
+            shopt -s huponexit 2>/dev/null || true && \
+            $*"
+    fi
 else
     kubectl exec ${POD_NAME} -c engine -- /bin/bash -lc "
         cat /etc/hosts > /dev/null || true && \
