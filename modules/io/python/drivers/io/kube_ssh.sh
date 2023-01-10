30127c1b547d6a06a006b7cb5325d8de6869dec4
Ye Cao <caoye.cao@alibaba-inc.com>
2023-01-10 19:23:29 +0800

    Reflactor the shell code
    Signed-off-by: Ye Cao <caoye.cao@alibaba-inc.com>

--- modules/io/python/drivers/io/kube_ssh.sh
+++ modules/io/python/drivers/io/kube_ssh.sh
@@ -3,32 +3,21 @@
 POD_NAME=$1
 shift
 
+KUBE_NS="default"
+KUBE_POD_NAME=""
+KUBE_CONTAINER_NAME=""
 if [[ "$POD_NAME" =~ ":" ]]; then
     KUBE_NS=${POD_NAME%%:*}
     KUBE_POD_CONTAINER_NAME=${POD_NAME#*:}
     if [[ "$KUBE_POD_CONTAINER_NAME" =~ ":" ]]; then
         KUBE_POD_NAME=${KUBE_POD_CONTAINER_NAME%%:*}
-        KUBE_CONTAINER_NAME=${KUBE_POD_CONTAINER_NAME#*:}
-        kubectl -n ${KUBE_NS} exec ${KUBE_POD_NAME} -c ${KUBE_CONTAINER_NAME} -- /bin/bash -lc "
-            cat /etc/hosts > /dev/null || true && \
-            source /etc/profile || true && \
-            source /etc/bash.bashrc || true && \
-            source ~/.profile || true && \
-            source ~/.bashrc || true && \
-            shopt -s huponexit 2>/dev/null || true && \
-            $*"
+        KUBE_CONTAINER_NAME="-c "${KUBE_POD_CONTAINER_NAME#*:}
     else
-        kubectl -n ${KUBE_NS} exec ${KUBE_POD_CONTAINER_NAME} -c engine -- /bin/bash -lc "
-            cat /etc/hosts > /dev/null || true && \
-            source /etc/profile || true && \
-            source /etc/bash.bashrc || true && \
-            source ~/.profile || true && \
-            source ~/.bashrc || true && \
-            shopt -s huponexit 2>/dev/null || true && \
-            $*"
+        KUBE_POD_NAME=${KUBE_POD_CONTAINER_NAME}
     fi
-else
-    kubectl exec ${POD_NAME} -c engine -- /bin/bash -lc "
+fi
+
+kubectl -n ${KUBE_NS} exec ${KUBE_POD_NAME} ${KUBE_CONTAINER_NAME} -- /bin/bash -lc "
         cat /etc/hosts > /dev/null || true && \
         source /etc/profile || true && \
         source /etc/bash.bashrc || true && \
@@ -36,4 +25,3 @@ else
         source ~/.bashrc || true && \
         shopt -s huponexit 2>/dev/null || true && \
         $*"
-fi
