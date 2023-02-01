v6d-io/v6d

commit 7c09843102081e9c1c9fabd945f493195c31752b

Bump up vineyard version to v0.12.1
Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

CMakeLists.txt
@@ -2,7 +2,7 @@ cmake_minimum_required(VERSION 3.3)
 
 set(VINEYARD_MAJOR_VERSION 0)
 set(VINEYARD_MINOR_VERSION 12)
-set(VINEYARD_PATCH_VERSION 0)
+set(VINEYARD_PATCH_VERSION 1)
 set(VINEYARD_VERSION ${VINEYARD_MAJOR_VERSION}.${VINEYARD_MINOR_VERSION}.${VINEYARD_PATCH_VERSION})
 
 message(STATUS "Configuring and building vineyard version '${VINEYARD_VERSION}'.")

charts/vineyard-operator/Chart.yaml
@@ -24,10 +24,10 @@ type: application
 # This is the chart version. This version number should be incremented each time you make changes
 # to the chart and its templates, including the app version.
 # Versions are expected to follow Semantic Versioning (https://semver.org/)
-version: 0.12.0
+version: 0.12.1
 
 # This is the version number of the application being deployed. This version number should be
 # incremented each time you make changes to the application. Versions are not expected to
 # follow Semantic Versioning. They should reflect the version the application is using.
-appVersion: 0.12.0
+appVersion: 0.12.1
 

setup.cfg
@@ -1,5 +1,5 @@
 [metadata]
-version = 0.12.0
+version = 0.12.1
 
 [pycodestyle]
 max_line_length = 88

src/common/util/config.h
@@ -18,11 +18,11 @@ limitations under the License.
 
 #define VINEYARD_VERSION_MAJOR 0
 #define VINEYARD_VERSION_MINOR 12
-#define VINEYARD_VERSION_PATCH 0
+#define VINEYARD_VERSION_PATCH 1
 
 #define VINEYARD_VERSION                                              \
   ((VINEYARD_VERSION_MAJOR * 1000) + VINEYARD_VERSION_MINOR) * 1000 + \
       VINEYARD_VERSION_PATCH
-#define VINEYARD_VERSION_STRING "0.12.0"
+#define VINEYARD_VERSION_STRING "0.12.1"
 
 #endif  // SRC_COMMON_UTIL_CONFIG_H_

src/server/async/socket_server.cc
@@ -446,6 +446,7 @@ bool SocketConnection::doGetRemoteBuffers(const json& root) {
   bool compress = false;
   std::vector<std::shared_ptr<Payload>> objects;
   std::string message_out;
+  this->registered_.store(false);
 
   TRY_READ_REQUEST(ReadGetRemoteBuffersRequest, root, ids, unsafe, compress);
   RESPONSE_ON_ERROR(bulk_store_->GetUnsafe(ids, unsafe, objects));

src/server/async/socket_server.h
@@ -219,7 +219,7 @@ class SocketConnection : public std::enable_shared_from_this<SocketConnection> {
   }
 
   // whether the connection has been correctly "registered"
-  std::atomic_bool registered_ = false;
+  std::atomic_bool registered_;
 
   stream_protocol::socket socket_;
   std::shared_ptr<VineyardServer> server_ptr_;

commit ba2d0839706f8fa678fa80f810c71205f12a223a

Bump up vineyard version to v0.12.2
Fixes a bug introduced in https://github.com/v6d-io/v6d/commit/7c09843102081e9c1c9fabd945f493195c31752b

Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

.github/workflows/build-vineyardd-and-wheels-linux.yaml
@@ -154,9 +154,9 @@ jobs:
         run: |
           if [[ "${{ matrix.python }}" == "cp311-cp311" ]];
           then
-            export base=vineyard-manylinux2014:20230115
+            export base=manylinux2014:20230115
           else
-            export base=vineyard-manylinux1:20220512
+            export base=manylinux1:20220512
           fi
 
           sudo docker build . -f docker/pypa/Dockerfile.manylinux1-wheel \

CMakeLists.txt
@@ -2,7 +2,7 @@ cmake_minimum_required(VERSION 3.3)
 
 set(VINEYARD_MAJOR_VERSION 0)
 set(VINEYARD_MINOR_VERSION 12)
-set(VINEYARD_PATCH_VERSION 1)
+set(VINEYARD_PATCH_VERSION 2)
 set(VINEYARD_VERSION ${VINEYARD_MAJOR_VERSION}.${VINEYARD_MINOR_VERSION}.${VINEYARD_PATCH_VERSION})
 
 message(STATUS "Configuring and building vineyard version '${VINEYARD_VERSION}'.")

charts/vineyard-operator/Chart.yaml
@@ -24,10 +24,10 @@ type: application
 # This is the chart version. This version number should be incremented each time you make changes
 # to the chart and its templates, including the app version.
 # Versions are expected to follow Semantic Versioning (https://semver.org/)
-version: 0.12.1
+version: 0.12.2
 
 # This is the version number of the application being deployed. This version number should be
 # incremented each time you make changes to the application. Versions are not expected to
 # follow Semantic Versioning. They should reflect the version the application is using.
-appVersion: 0.12.1
+appVersion: 0.12.2
 

setup.cfg
@@ -1,5 +1,5 @@
 [metadata]
-version = 0.12.1
+version = 0.12.2
 
 [pycodestyle]
 max_line_length = 88

src/common/util/config.h
@@ -18,11 +18,11 @@ limitations under the License.
 
 #define VINEYARD_VERSION_MAJOR 0
 #define VINEYARD_VERSION_MINOR 12
-#define VINEYARD_VERSION_PATCH 1
+#define VINEYARD_VERSION_PATCH 2
 
 #define VINEYARD_VERSION                                              \
   ((VINEYARD_VERSION_MAJOR * 1000) + VINEYARD_VERSION_MINOR) * 1000 + \
       VINEYARD_VERSION_PATCH
-#define VINEYARD_VERSION_STRING "0.12.1"
+#define VINEYARD_VERSION_STRING "0.12.2"
 
 #endif  // SRC_COMMON_UTIL_CONFIG_H_

src/server/async/socket_server.cc
@@ -50,6 +50,8 @@ SocketConnection::SocketConnection(
   if (plasma_bulk_store != nullptr) {
     plasma_bulk_store_ = plasma_bulk_store->shared_from_this();
   }
+  // initializing
+  this->registered_.store(false);
 }
 
 bool SocketConnection::Start() {
@@ -446,7 +448,6 @@ bool SocketConnection::doGetRemoteBuffers(const json& root) {
   bool compress = false;
   std::vector<std::shared_ptr<Payload>> objects;
   std::string message_out;
-  this->registered_.store(false);
 
   TRY_READ_REQUEST(ReadGetRemoteBuffersRequest, root, ids, unsafe, compress);
   RESPONSE_ON_ERROR(bulk_store_->GetUnsafe(ids, unsafe, objects));

test/remote_buffer_test.cc
@@ -57,7 +57,7 @@ void RemoteCreateTest(Client& ipc_client, RPCClient& rpc_client) {
     CHECK_EQ(local_buffer->data()[index], array_data[index]);
   }
 
-  LOG(INFO) << "Passed remote buffer (local create & remote get) tests...";
+  LOG(INFO) << "Passed remote buffer (remote create & local get) tests...";
 }
 
 void RemoteGetTest(Client& ipc_client, RPCClient& rpc_client) {