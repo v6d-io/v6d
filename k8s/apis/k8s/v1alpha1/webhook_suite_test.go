55a22bc750346bad6736ffd236b241ae7e72c6e4
Ye Cao <caoye.cao@alibaba-inc.com>
2023-02-13 18:29:41 +0800

    Bump up several components including golang, k8s api and controller-runtime (#1187)
    Bump up several components:
    * Bump up golang to 1.19
    * Bump up k8s api to 0.26.1
    * Bump up controller-runtime to 0.14.4
    * Bump up controller-tools to 0.11.0
    * delete the `webhook_suit_test.go` and `suite_test.go` as we already have e2e test.
    
    Fixed #1184
    
    Signed-off-by: Ye Cao <caoye.cao@alibaba-inc.com>

--- k8s/apis/k8s/v1alpha1/webhook_suite_test.go
+++ k8s/apis/k8s/v1alpha1/webhook_suite_test.go
@@ -1,11 +1,8 @@
 /** Copyright 2020-2023 Alibaba Group Holding Limited.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,7 +31,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
+
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
@@ -51,9 +48,8 @@ var cancel context.CancelFunc
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	RunSpecsWithDefaultAndCustomReporters(t,
-		"Webhook Suite",
-		[]Reporter{printer.NewlineReporter{}})
+	RunSpecs(t,
+		"Webhook Suite")
 }
 
 var _ = BeforeSuite(func() {
