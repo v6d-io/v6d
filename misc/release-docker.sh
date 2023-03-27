648d9f0da3e3c4ed956365ef533e043bc2428a34
Tao He <linzhu.ht@alibaba-inc.com>
2023-03-27 10:15:29 +0800

    Fixes macos M1 CI
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

--- misc/release-docker.sh
+++ misc/release-docker.sh
@@ -11,14 +11,22 @@ if [ "$version" = "" ]; then
     exit 1
 fi
 
-docker pull ghcr.io/v6d-io/v6d/vineyardd:$version
-docker pull ghcr.io/v6d-io/v6d/vineyardd:latest
-docker pull ghcr.io/v6d-io/v6d/vineyard-operator:nightly
+# vineyardd
+for tag in ${version} latest alpine-latest; do
+    for arch in x86_64 aarch64; do
+        docker pull ghcr.io/v6d-io/v6d/vineyardd:${tag}_${arch}
+        docker tag ghcr.io/v6d-io/v6d/vineyardd:${tag}_${arch} vineyardcloudnative/vineyardd:${tag}_${arch}
+        docker push vineyardcloudnative/vineyardd:${tag}_${arch}
+    done
 
-docker tag ghcr.io/v6d-io/v6d/vineyardd:$version vineyardcloudnative/vineyardd:$version
-docker tag ghcr.io/v6d-io/v6d/vineyardd:latest vineyardcloudnative/vineyardd:latest
-docker tag ghcr.io/v6d-io/v6d/vineyard-operator:nightly vineyardcloudnative/vineyard-operator:nightly
+    docker manifest create vineyardcloudnative/vineyardd:${tag} \
+        --amend vineyardcloudnative/vineyardd:${tag}_x86_64 \
+        --amend vineyardcloudnative/vineyardd:${tag}_aarch64
+    docker manifest push vineyardcloudnative/vineyardd:${tag}
+done
 
-docker push vineyardcloudnative/vineyardd:$version
-docker push vineyardcloudnative/vineyardd:latest
-docker push vineyardcloudnative/vineyard-operator:nightly
+# vineyard-operator
+for tag in ${version} latest; do
+    # see also: https://stackoverflow.com/questions/68317302/is-it-possible-to-copy-a-multi-os-image-from-one-docker-registry-to-another-on-a
+    regctl image copy -v info ghcr.io/v6d-io/v6d/vineyard-operator:${tag} vineyardcloudnative/vineyard-operator:${tag}
+done
