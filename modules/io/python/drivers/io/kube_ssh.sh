7c11ed08c2dd40e77b370f577b1a7f87722bad4e
siyuan0322 <siyuanzhang.zsy@alibaba-inc.com>
2023-03-31 12:40:24 +0800

    allow pass options from uri
    Signed-off-by: siyuan0322 <siyuanzhang.zsy@alibaba-inc.com>

--- modules/io/python/drivers/io/kube_ssh.sh
+++ modules/io/python/drivers/io/kube_ssh.sh
@@ -3,11 +3,11 @@
 POD_NAME=$1
 shift
 
-KUBE_NS="default"
-KUBE_POD_NAME=""
+KUBE_NS=""
+KUBE_POD_NAME=${POD_NAME}
 KUBE_CONTAINER_NAME=""
 if [[ "$POD_NAME" =~ ":" ]]; then
-    KUBE_NS=${POD_NAME%%:*}
+    KUBE_NS="-n "${POD_NAME%%:*}
     KUBE_POD_CONTAINER_NAME=${POD_NAME#*:}
     if [[ "$KUBE_POD_CONTAINER_NAME" =~ ":" ]]; then
         KUBE_POD_NAME=${KUBE_POD_CONTAINER_NAME%%:*}
@@ -17,7 +17,7 @@ if [[ "$POD_NAME" =~ ":" ]]; then
     fi
 fi
 
-kubectl -n ${KUBE_NS} exec ${KUBE_POD_NAME} ${KUBE_CONTAINER_NAME} -- /bin/bash -lc "
+kubectl ${KUBE_NS} exec ${KUBE_POD_NAME} ${KUBE_CONTAINER_NAME} -- /bin/bash -lc "
         cat /etc/hosts > /dev/null || true && \
         source /etc/profile || true && \
         source /etc/bash.bashrc || true && \
