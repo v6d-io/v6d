51bc511da709edcb41c7b9ce4b8695ba2762605d
Tao He <linzhu.ht@alibaba-inc.com>
2023-05-24 15:36:05 +0800

    Remove the ctti dependency (#1394)
    Part of #1376 and #1379.
    
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

--- modules/basic/stream/parallel_stream.cc
+++ modules/basic/stream/parallel_stream.cc
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include "basic/stream/parallel_stream.h"
+#include "basic/stream/parallel_stream.h"  // NOLINT(build/include)
 
 #include <memory>
 #include <string>
