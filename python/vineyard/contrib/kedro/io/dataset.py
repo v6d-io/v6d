a0b6fe3cc20d92d221e74c612512e514c895ebb5
Tao He <linzhu.ht@alibaba-inc.com>
2023-05-25 13:10:17 +0800

    Kedro integration: the vineyardataset, plugin and cli tools (#1287)
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

---
+++ python/vineyard/contrib/kedro/io/dataset.py
@@ -0,0 +1,114 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020-2023 Alibaba Group Holding Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""``VineyardDataSet`` is a data set implementation which handles in-memory data.
+   stored in vineyard.
+"""
+
+import functools
+import logging
+import time
+from typing import Any
+from typing import Dict
+
+from kedro.io.core import AbstractDataSet
+
+_EMPTY = object()
+
+
+class VineyardDataSet(AbstractDataSet):
+    """``Vineyard`` loads and saves Python objects from/to vineyard.
+
+    Example:
+    ::
+
+        >>> from vineyard.contrib.kedro.io import VineyardDataSet
+        >>> import pandas as pd
+        >>>
+        >>> data = pd.DataFrame({'col1': [1, 2], 'col2': [4, 5],
+        >>>                      'col3': [5, 6]})
+        >>> data_set = VineyardDataSet(ds_name="data")
+        >>>
+        >>> data_set.save(data)
+        >>> loaded_data = data_set.load()
+        >>> assert loaded_data.equals(data)
+        >>>
+        >>> new_data = pd.DataFrame({'col1': [1, 2], 'col2': [4, 5]})
+        >>> data_set.save(new_data)
+        >>> reloaded_data = data_set.load()
+        >>> assert reloaded_data.equals(new_data)
+
+    """
+
+    def __init__(self, ds_name: str, vineyard_ipc_socket: str = None):
+        """Creates a new instance of ``MemoryDataSet`` pointing to the
+        provided Python object.
+
+        Args:
+            data: Python object containing the data.
+            copy_mode: The copy mode used to copy the data. Possible
+                values are: "deepcopy", "copy" and "assign". If not
+                provided, it is inferred based on the data type.
+        """
+        self.ds_name = ds_name
+        self.vineyard_ipc_socket = vineyard_ipc_socket
+
+        # vineyard client: lazy initialization
+        self._client = None
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        # Remove the unpicklable entries.
+        del state['_client']
+        return state
+
+    def _init_client(self):
+        if self._client is None:
+            import vineyard
+
+            self._client: vineyard.IPCClient = vineyard.connect(
+                self.vineyard_ipc_socket
+            )
+
+    def _load(self) -> Any:
+        self._init_client()
+        return self._client.get(name=self.ds_name, fetch=True)
+
+    def _save(self, data: Any):
+        self._init_client()
+        self._release()  # release the (possible) old data
+        self._client.put(data, name=self.ds_name, persist=True)
+
+    def _exists(self) -> bool:
+        self._init_client()
+        try:
+            return self._client.exists(self._client.get_name(self.ds_name))
+        except:  # noqa: E722, pylint: disable=bare-except
+            return False
+
+    def _release(self) -> None:
+        self._init_client()
+        try:
+            object_id = self._client.get_name(self.ds_name)
+        except:  # noqa: E722, pylint: disable=bare-except
+            return
+        self._client.delete(object_id)
+        self._client.drop_name(self.ds_name)
+
+    def _describe(self) -> Dict[str, Any]:
+        return {"name": self.ds_name, "vineyard_ipc_socket": self.vineyard_ipc_socket}
---
+++ setup_kedro.py
@@ -0,0 +1,117 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020-2023 Alibaba Group Holding Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+from setuptools.command.install import install
+from wheel.bdist_wheel import bdist_wheel
+
+
+class bdist_wheel_plat(bdist_wheel):
+    def finalize_options(self):
+        bdist_wheel.finalize_options(self)
+        self.root_is_pure = False
+
+    def get_tag(self):
+        self.root_is_pure = True
+        tag = bdist_wheel.get_tag(self)
+        self.root_is_pure = False
+        return tag
+
+
+class install_plat(install):
+    def finalize_options(self):
+        self.install_lib = self.install_platlib
+        install.finalize_options(self)
+
+
+def find_airflow_packages(root):
+    pkgs = []
+    for pkg in find_packages(root):
+        if 'contrib.kedro' in pkg:
+            pkgs.append(pkg)
+    return pkgs
+
+
+with open(
+    os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        'python',
+        'vineyard',
+        'contrib',
+        'kedro',
+        'README.md',
+    ),
+    encoding='utf-8',
+    mode='r',
+) as fp:
+    long_description = fp.read()
+
+setup(
+    name='vineyard-kedro',
+    author='The vineyard team',
+    author_email='developers@v6d.io',
+    description='Vineyard provider for kedro',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://v6d.io',
+    package_dir={'vineyard.contrib.kedro': 'python/vineyard/contrib/kedro'},
+    package_data={
+        'vineyard.contrib.kedro': ['*.yaml', '*.README'],
+    },
+    packages=find_airflow_packages('python'),
+    entry_points={
+        "kedro.global_commands": ["vineyard = vineyard.contrib.kedro.plugins.cli:cli"],
+        # don't auto- register the hook
+        # "kedro.hooks": ["vineyard = vineyard.contrib.kedro.plugins.hook:hooks"],
+        # "kedro.starters": ["starter = vineyard.contrib.kedro.plugins.starter:starters"],
+    },
+    cmdclass={'bdist_wheel': bdist_wheel_plat, "install": install_plat},
+    zip_safe=False,
+    install_requires=[
+        'kedro',
+        'vineyard',
+    ],
+    platforms=['POSIX', 'MacOS'],
+    license="Apache License 2.0",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Distributed Computing",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    project_urls={
+        'Documentation': 'https://v6d.io',
+        'Source': 'https://github.com/v6d-io/v6d',
+        'Tracker': 'https://github.com/v6d-io/v6d/issues',
+    },
+)
