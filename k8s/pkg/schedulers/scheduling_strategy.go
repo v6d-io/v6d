684e4e8447b7a14889405d1c20f92e834c128361
Ye Cao <caoye.cao@alibaba-inc.com>
2023-05-30 16:19:43 +0800

    Fix a bug to avoid the panic 'interger divide by zero'.
    Signed-off-by: Ye Cao <caoye.cao@alibaba-inc.com>

--- k8s/pkg/schedulers/scheduling_strategy.go
+++ k8s/pkg/schedulers/scheduling_strategy.go
@@ -54,6 +54,9 @@ func NewRoundRobinStrategy(nodes []string) *RoundRobinStrategy {
 // Compute returns the node by the given rank.
 func (r *RoundRobinStrategy) Compute(rank int) (string, error) {
 	l := len(r.nodes)
+	if l == 0 {
+		return "", nil
+	}
 	return r.nodes[rank%l], nil
 }
 
