bc94e6a0194c9fa8a45e9d169bfa33e7b4439612
Tao He <linzhu.ht@alibaba-inc.com>
2023-12-26 15:50:18 +0800

    Bump up vineyard version to v0.19.3
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

--- k8s/cmd/main.go
+++ k8s/cmd/main.go
@@ -48,7 +48,7 @@ var cmdLong = util.LongDesc(`
 
 var cmd = &cobra.Command{
 	Use:     "vineyardctl [command]",
-	Version: "v0.19.2",
+	Version: "v0.19.3",
 	Short:   "vineyardctl is the command-line tool for interact with the Vineyard Operator.",
 	Long:    cmdLong,
 }
