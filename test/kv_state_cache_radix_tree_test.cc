5065f3b47b8f77600a7a816fb79071b26917364c
Ye Cao <caoye.cao@alibaba-inc.com>
2024-02-05 19:14:40 +0800

    Add the unit test for the radix tree. (#1749)
    Fixes #1747
    
    Signed-off-by: Ye Cao <caoye.cao@alibaba-inc.com>

---
+++ test/kv_state_cache_radix_tree_test.cc
@@ -0,0 +1,189 @@
+/** Copyright 2020-2023 Alibaba Group Holding Limited.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <unistd.h>
+#include <iostream>
+#include <random>
+#include <vector>
+#include "kv-state-cache/radix-tree/radix.h"
+
+#include "common/util/logging.h"
+#include "kv-state-cache/utils/kv_state_cache_utils.h"
+
+using namespace vineyard;
+
+void print_tokens(const std::vector<int>& tokens) {
+  std::string tokens_str = "";
+  for (size_t i = 0; i < tokens.size(); ++i) {
+    tokens_str += std::to_string(tokens[i]);
+  }
+  LOG(INFO) << "Current tokens: " + tokens_str;
+}
+
+void radix_tree_insert_test() {
+    std::shared_ptr<RadixTree> radix_tree = std::make_shared<RadixTree>(10);
+
+    /* insert a token list*/
+    std::vector<int> tokens;
+    std::shared_ptr<NodeData> node_data;
+    for (int i = 0; i < 10; i++) {
+        tokens.push_back(i);
+        VINEYARD_ASSERT(radix_tree->Insert(tokens, node_data) != NULL);
+        VINEYARD_ASSERT(node_data == NULL);
+    }
+
+    /* insert new token and check whether the old token is evicted */ 
+    tokens.clear();
+    for (int i = 1; i < 10; i++) {
+        tokens.push_back(i);
+        VINEYARD_ASSERT(radix_tree->Insert(tokens, node_data) != NULL);
+        VINEYARD_ASSERT(node_data != NULL);
+    }
+
+    /* insert a token that prefix is not in the radix tree */
+    tokens.clear();
+    for (int i = 10; i > 0; i--) {
+        tokens.push_back(i);
+    }
+    VINEYARD_ASSERT(radix_tree->Insert(tokens, node_data) == NULL);
+}
+
+void radix_tree_delete_test(){
+    std::shared_ptr<RadixTree> radix_tree = std::make_shared<RadixTree>(10);
+
+    /* insert a token list*/
+    std::vector<int> tokens;
+    std::shared_ptr<NodeData> node_data;
+    for (int i = 0; i < 10; i++) {
+        tokens.push_back(i);
+        VINEYARD_ASSERT(radix_tree->Insert(tokens, node_data) != NULL);
+        VINEYARD_ASSERT(node_data == NULL);
+    }
+
+    /* delete a token list*/
+
+    tokens.clear();
+    node_data = NULL;
+    for (int i = 0; i < 5; i++) {
+        tokens.push_back(i);
+    }
+    radix_tree->Delete(tokens, node_data);
+    VINEYARD_ASSERT(node_data != NULL);
+
+    /* delete a token list that is not in the radix tree */
+    tokens.clear();
+    node_data = NULL;
+    for (int i = 10; i > 0; i--) {
+        tokens.push_back(i);
+    }
+    radix_tree->Delete(tokens, node_data);
+    VINEYARD_ASSERT(node_data == NULL);
+}
+
+void radix_tree_query_test() {
+    std::shared_ptr<RadixTree> radix_tree = std::make_shared<RadixTree>(10);
+
+    /* insert a token list*/
+    std::vector<int> tokens;
+    std::shared_ptr<NodeData> node_data;
+    for (int i = 0; i < 10; i++) {
+        tokens.push_back(i);
+        VINEYARD_ASSERT(radix_tree->Insert(tokens, node_data) != NULL);
+        VINEYARD_ASSERT(node_data == NULL);
+    }
+
+    /* query a token list*/
+    tokens.clear();
+    for (int i = 0; i < 5; i++) {
+        tokens.push_back(i);
+    }
+    VINEYARD_ASSERT(radix_tree->Query(tokens) != NULL);
+
+    /* query a token list that is not in the radix tree */
+    tokens.clear();
+    for (int i = 10; i > 0; i--) {
+        tokens.push_back(i);
+    }
+    VINEYARD_ASSERT(radix_tree->Query(tokens) == NULL);
+}
+
+void radix_tree_serialize_and_deserailize() {
+    std::shared_ptr<RadixTree> radix_tree = std::make_shared<RadixTree>(10);
+
+    /* insert a token list*/
+    std::vector<int> tokens;
+    std::shared_ptr<NodeData> node_data;
+    for (int i = 0; i < 10; i++) {
+        tokens.push_back(i);
+        VINEYARD_ASSERT(radix_tree->Insert(tokens, node_data) != NULL);
+        VINEYARD_ASSERT(node_data == NULL);
+    }
+
+    /* serialize radix tree */
+    std::string serialized_radix_tree = radix_tree->Serialize();
+
+    /* deserialize radix tree */
+    std::shared_ptr<RadixTree> deserialized_radix_tree = radix_tree->Deserialize(serialized_radix_tree);
+
+    /* query to check whether all token list exist */
+    tokens.clear();
+    for (int i = 0; i < 10; i++) {
+        tokens.push_back(i);
+        print_tokens(tokens);
+        VINEYARD_ASSERT(deserialized_radix_tree->Query(tokens) != NULL);
+    }
+}
+
+void radix_tree_split() {
+    std::shared_ptr<RadixTree> radix_tree = std::make_shared<RadixTree>(20);
+
+    raxShow(radix_tree->tree);
+    /* insert a token list*/
+    std::vector<int> tokens;
+    std::shared_ptr<NodeData> node_data;
+    for (int i = 0; i < 10; i++) {
+        tokens.push_back(i);
+        VINEYARD_ASSERT(radix_tree->Insert(tokens, node_data) != NULL);
+        VINEYARD_ASSERT(node_data == NULL);
+    }
+
+    /* split a token list*/
+    tokens.clear();
+    for (int i = 0; i < 5; i++) {
+        tokens.push_back(i);
+    }
+    print_tokens(tokens);
+    std::shared_ptr<NodeData> subTreeHeader;
+    std::vector<std::shared_ptr<NodeData>> node_data_list = radix_tree->Split(tokens, subTreeHeader);
+    VINEYARD_ASSERT(node_data_list.size() == 7);
+}
+
+int main() {
+    LOG(INFO) << "Start to test radix tree insert...";
+    radix_tree_insert_test();
+    LOG(INFO) << "Finish radix tree insert test!";
+    LOG(INFO) << "Start to test radix tree delete...";
+    radix_tree_delete_test();
+    LOG(INFO) << "Finish radix tree delete test!";
+    LOG(INFO) << "Start to test radix tree query...";
+    radix_tree_query_test();
+    LOG(INFO) << "Finish radix tree query test!";
+    LOG(INFO) << "Start to test radix tree serialize and deserialize...";
+    radix_tree_serialize_and_deserailize();
+    LOG(INFO) << "Finish radix tree serialize and deserialize test!";
+    LOG(INFO) << "Start to test radix tree split...";
+    radix_tree_split();
+    LOG(INFO) << "Finish radix tree split test!";
+}
