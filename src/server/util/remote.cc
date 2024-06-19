441d852904341892280b7933986bad3f01181acc
vegetableysm <yuanshumin.ysm@alibaba-inc.com>
2024-06-19 19:58:02 +0800

    Fix migrate buffer bug.
    Signed-off-by: vegetableysm <yuanshumin.ysm@alibaba-inc.com>

--- src/server/util/remote.cc
+++ src/server/util/remote.cc
@@ -606,8 +606,12 @@ void ReceiveRemoteBuffers(asio::generic::stream_protocol::socket& socket,
 
       const auto& object = objects[index];
       if (!object || !object->pointer) {
-        VINEYARD_DISCARD(callback_after_finish(
-            Status::IOError("Object or pointer is null")));
+        if (object->data_size > 0) {
+          VINEYARD_DISCARD(callback_after_finish(
+              Status::IOError("Object or pointer is null")));
+          return;
+        }
+        process_next(socket);
         return;
       }
 
