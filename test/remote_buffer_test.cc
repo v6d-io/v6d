af4e29204023fc8eb0bcb8c63acc05c74c5b1a2f
vegetableysm <yuanshumin.ysm@alibaba-inc.com>
2024-06-24 11:00:09 +0800

    Share code.
    Signed-off-by: vegetableysm <yuanshumin.ysm@alibaba-inc.com>

--- test/remote_buffer_test.cc
+++ test/remote_buffer_test.cc
@@ -28,9 +28,6 @@ limitations under the License.
 
 using namespace vineyard;  // NOLINT(build/namespaces)
 
-constexpr uint64_t element_num =
-    (uint64_t) 1024 * 1024 * 1024 * 16 / sizeof(uint64_t);
-
 void RemoteCreateTest(Client& ipc_client, RPCClient& rpc_client) {
   std::vector<double> double_array = {1.0, 7.0, 3.0, 4.0, 2.0};
   char* array_data = reinterpret_cast<char*>(double_array.data());
@@ -136,72 +133,25 @@ void RemoteCreateAndGetTest(Client& ipc_client, RPCClient& rpc_client) {
   LOG(INFO) << "Passed remote buffer (remote create & remote get) tests...";
 }
 
-void RemoteBigArrayCreateAndGetTest(Client& ipc_client, RPCClient& rpc_client) {
-  std::vector<uint64_t> big_array_vec;
-  for (size_t i = 0; i < element_num; ++i) {
-    big_array_vec.push_back(i + 1000);
-  }
-  uint64_t const* array_data = big_array_vec.data();
-
-  // create remote buffer
-  auto remote_blob_writer = std::make_shared<RemoteBlobWriter>(
-      big_array_vec.size() * sizeof(uint64_t));
-  std::memcpy(remote_blob_writer->data(), array_data,
-              big_array_vec.size() * sizeof(uint64_t));
-  ObjectMeta blob_meta;
-  VINEYARD_CHECK_OK(rpc_client.CreateRemoteBlob(remote_blob_writer, blob_meta));
-  ObjectID blob_id = blob_meta.GetId();
-  CHECK_NE(blob_id, InvalidObjectID());
-
-  LOG(INFO) << "created blob: " << ObjectIDToString(blob_id);
-
-  // get remote buffer
-  std::shared_ptr<RemoteBlob> remote_buffer;
-  VINEYARD_CHECK_OK(rpc_client.GetRemoteBlob(blob_id, remote_buffer));
-
-  // check the remote buffer
-  CHECK_EQ(remote_buffer->id(), blob_id);
-  CHECK_EQ(remote_buffer->instance_id(), ipc_client.instance_id());
-  CHECK_EQ(remote_buffer->instance_id(), rpc_client.remote_instance_id());
-  CHECK_EQ(remote_buffer->allocated_size(),
-           big_array_vec.size() * sizeof(uint64_t));
-
-  // compare the buffer contents
-  for (size_t index = 0; index < element_num; ++index) {
-    CHECK_EQ((reinterpret_cast<const uint64_t*>(remote_buffer->data()))[index],
-             array_data[index]);
-  }
-
-  LOG(INFO)
-      << "Passed remote big size buffer (remote create & remote get) tests...";
-}
-
 int main(int argc, char** argv) {
   if (argc < 3) {
-    printf(
-        "usage ./remote_buffer_test <ipc_socket> <rpc_endpoint> "
-        "<rdma_endpoint>");
+    printf("usage ./remote_buffer_test <ipc_socket> <rpc_endpoint>");
     return 1;
   }
   std::string ipc_socket(argv[1]);
   std::string rpc_endpoint(argv[2]);
-  std::string rdma_endpoint = "";
-  if (argc > 3) {
-    rdma_endpoint = std::string(argv[3]);
-  }
 
   Client ipc_client;
   VINEYARD_CHECK_OK(ipc_client.Connect(ipc_socket));
   LOG(INFO) << "Connected to IPCServer: " << ipc_socket;
 
   RPCClient rpc_client;
-  VINEYARD_CHECK_OK(rpc_client.Connect(rpc_endpoint, "", "", rdma_endpoint));
+  VINEYARD_CHECK_OK(rpc_client.Connect(rpc_endpoint));
   LOG(INFO) << "Connected to RPCServer: " << rpc_endpoint;
 
   RemoteCreateTest(ipc_client, rpc_client);
   RemoteGetTest(ipc_client, rpc_client);
   RemoteCreateAndGetTest(ipc_client, rpc_client);
-  RemoteBigArrayCreateAndGetTest(ipc_client, rpc_client);
 
   LOG(INFO) << "Passed remote buffer tests...";
 
